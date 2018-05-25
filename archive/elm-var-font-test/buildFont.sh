370ca981920a1435bac9cc420f095e002e69a22e
Stephen Nixon <swr410@gmail.com>
2018-05-25 16:35:58 +0200

    move archive outside git repo

--- archive/elm-var-font-test/buildFont.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-folder=RomanMasters
-font=elm-mono
-
-# build the OTF version -- this requires the AFDKO toolkit
-# which is available at https://github.com/adobe-type-tools/afdko
-buildmasterotfs $folder/$font.designspace
-buildcff2vf $folder/$font.designspace
