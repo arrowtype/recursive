cf4ded120371ba952e0c9ddb50d92fc4617d9c52
Stephen Nixon <swr410@gmail.com>
2018-06-23 15:22:45 +0200

    improving interpolation diagram

--- drawbot/interpolation-diagrams/draw-glyph-interpolation-robofont_drawbot-bullet.py
+++ drawbot/interpolation-diagrams/draw-glyph-interpolation-robofont_drawbot-bullet.py
@@ -1,30 +1,46 @@
 f = CurrentFont()
-
-glyphName = "bullet"
+rect(0,0,1000,1000)
+# glyphName = "bullet"
+glyphName = "S"
 
 pointsDict = {}
 
+def normalRGB(r, g, b):
+    r1, g1, b1 = r / 255, g / 255, b / 255
+    return((r1, g1, b1))
+
+#TODO: animate, with axis values
+
 # TODO: it would be better to grab the glyph box for this
-glyph1Pos = (400,400) #(22, 90)
-glyph2Pos = (400,400) #(672, 590)
+glyph1Pos = (-14, 58) #(200,200) 
+glyph2Pos = (566, 416) # (200,200)
 
 glyphNum =0
 
-scale(.75)
+scale(.85)
 
-## TODO: make into a funtion(?) to put second cap on top
+## TODO: make into a function(?) to put second cap on top
 ## currently works okay for cap in two fonts
 for f in AllFonts():
 
     save()
+    strokeWidth(2)
     if glyphNum == 0:
         translate(glyph1Pos[0],glyph1Pos[1])
+        
+        glyphColor = normalRGB(83, 59, 230)
+        fill(glyphColor[0],glyphColor[1],glyphColor[2],0.2)
+        stroke(glyphColor[0],glyphColor[1],glyphColor[2],0.75)
+        
     if glyphNum == 1:
         translate(glyph2Pos[0],glyph2Pos[1])
+        
+        glyphColor = normalRGB(210, 46, 237)
+        fill(glyphColor[0],glyphColor[1],glyphColor[2],0.15)
+        stroke(glyphColor[0],glyphColor[1],glyphColor[2],0.6)
     # save()
     # scale(.75)
-    fill(0,0,1,.15)
-    stroke(0,0,1)
+   
     g = f[glyphName]
     drawGlyph(g)
     
@@ -34,8 +50,12 @@ for f in AllFonts():
             # print(p)
             # help(p)
             # for p in s:
-            fill(1,0,0,.25)
-            stroke(1,0,0)
+            pointsColor = normalRGB(210, 46, 237)
+            # fill(pointsColor[0],pointsColor[1],pointsColor[2],.5)
+            fill(1,1,1,.5)
+            strokeWidth(2)
+            # stroke(pointsColor[0],pointsColor[1],pointsColor[2])
+            stroke(1,1,1)
             if p.type == "curve" or p.type == "line":
                 oval(p.x-5,p.y-5,10,10)
 
@@ -50,7 +70,7 @@ for f in AllFonts():
                     pointsDict[pointName] = [(p.x, p.y)]
                 
             # if p.type == "offcurve":
-            #     fill(1,0,0)
+            #     fill(1,1,1)
             #     oval(p.x-2.5, p.y-2.5, 5, 5)
                 #TODO? draw handles
             # print(p.type)
@@ -63,7 +83,8 @@ for f in AllFonts():
 
 for point, coordinates in pointsDict.items():
         
-    stroke(0,0,0, .75)
+    # stroke(0.5,0.5,0.5, 1)
+    stroke(1,1,1, .75)
     
     x1, y1 = (coordinates[0][0] + glyph1Pos[0], coordinates[0][1] + glyph1Pos[1])
     
@@ -72,6 +93,9 @@ for point, coordinates in pointsDict.items():
     print((coordinates[0][0], coordinates[0][1]),(coordinates[1][0], coordinates[1][1]))
 
     lineDash(4)
+    strokeWidth(2)
     line((x1,y1),(x2,y2))
     
 print(pointsDict)
+
+# saveImage("recursive-interpolation-diagram-S.pdf")
