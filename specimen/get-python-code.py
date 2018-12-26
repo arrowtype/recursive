f1c54f92e10b8e233be30f4ca57125a35262bd37
Stephen Nixon <swr410@gmail.com>
2018-12-26 11:51:50 -0600

    upgrade python example getting

--- specimen/get-python-code.py
+++ specimen/get-python-code.py
@@ -1,30 +1,80 @@
-# import os
+# ------------------------------------------------------------------------------
+# set up variables -------------------------------------------------------------
 
-# dir_path = os.path.dirname(os.path.realpath(__file__))
+folderToGet = '../../../../Users/stephennixon/Environments/gfonts3/lib/python3.6'
+fileToSave = 'specimen/pythonCodeExamples.py'
 
-# print(dir_path)
+minWordLength = 2
+maxWordLength = 45
 
-folderToGet = "../../../../Users/stephennixon/Environments/gfonts3/lib/python3.6"
-
-# codeExample= ""
-
-# # for file in folder
-# for filename in os.listdir(folderToGet):
-#     if filename.endswith(".py"): 
-#         # print(os.path.join(directory, filename))
-#         file_object = open(folderToGet + "/" + filename, 'r')
-#         codeExample += file_object.read()
-#         # print(filename)
-
-# print(codeExample)
+# ------------------------------------------------------------------------------
+# set up basics ----------------------------------------------------------------
 
 from pathlib import Path
 
+f= open(fileToSave,"w+")
+
+codeExample= ""
+
+# ------------------------------------------------------------------------------
+# get text and add to string ---------------------------------------------------
+
 pathlist = Path(folderToGet).glob('**/*.py')
+
 for path in pathlist:
     # because path is object not string
     path_in_str = str(path)
-    print(path_in_str)
+    # print(path_in_str)
 
-    # open file
-    # add contents to loooong variable
+    file_object = open(path_in_str, 'r', encoding='utf-8')
+    codeExample += file_object.read()
+
+# ------------------------------------------------------------------------------
+# clean up string --------------------------------------------------------------
+
+glyphsToRemove = '" ( ) { } [ ] _ . ; = > < ? : , / - ` \ \' ^ @ 1 2 3 4 5 6 7 8 9 0'
+
+glyphsToRemoveList = glyphsToRemove.split(' ')
+
+cleanedCodeExample = ''
+
+
+for letter in codeExample:
+    if letter in glyphsToRemoveList:
+        cleanedCodeExample += ' '
+    else:
+        cleanedCodeExample += letter
+
+codeWords = set(cleanedCodeExample.replace('\n', ' ').replace('\t','').split(' '))
+
+# print(codeWords)
+
+wordsToRemove = ['',' ', '  ', '   ', '\t', 'aabcde1234567890', 'A4Landscape', 'molestie', 'vvvvvvvvvvvvvvvvvvvv', 'eEfFgGdiouxXcrs%',  'математика', 'numOps','bFamilyType','cmapLoading', 'ranges','bestDefault','compressedData']
+
+for word in wordsToRemove:
+    if word in codeWords:
+        codeWords.remove(word)
+
+
+sortedByLength = {}
+
+for index, word in enumerate(codeWords):
+    # print(word)
+    if len(word) >= minWordLength and len(word) <= maxWordLength:
+        if len(word) not in sortedByLength.keys():
+            sortedByLength[len(word)] = []
+        if len(word) in sortedByLength.keys():
+            sortedByLength[len(word)].append(word)
+            
+sortedByLengthSorted = {}
+
+for i in range(minWordLength,maxWordLength+1):
+    if i in sortedByLength.keys() and i % 1 == 0:
+        sortedByLengthSorted[i] = sortedByLength[i]
+
+# ------------------------------------------------------------------------------
+# write to file ----------------------------------------------------------------
+
+f.write('sortedByLengthSorted = ' + str(sortedByLengthSorted))
+
+f.close()
