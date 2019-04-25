2ea5c1933f0baf2412b29f9387b2ebc3aa889285
Stephen Nixon <swr410@gmail.com>
2019-04-25 16:11:08 -0400

    properly git mv filenames 'Gothic' -> 'Strict'

--- notes/converting-ufos-gothic_to_strict/git-mv-filenames.sh
+++ notes/converting-ufos-gothic_to_strict/git-mv-filenames.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# I have many UFO files which contain the style name "Gothic," which I have changed to "Strict."
-# This script will use `git mv` to update the style names as needed.
+# I have many UFO files which contain the style 
+# name "Gothic," which I have decided to change to "Strict."
+# This script will use `git mv` to update the style 
+# names as needed, without breaking repo history.
 
 set -ex
 
 find src/* -name '*Gothic*' -exec sh -c '
   for file do
-    git mv $file ${file/"Gothic"/"Strict"}
-    echo ----------------------------------
+    git mv "$file" "${file/'Gothic'/'Strict'}"
   done
 ' sh {} +
-
