f526903532328713f2606b6c53965e8a6e58e576
Stephen Nixon <stephen@thundernixon.com>
2019-05-21 16:52:25 -0400

    working on /w.italic

---
+++ src/00-recursive-scripts-for-robofont/copy-selected-glyph-foregrounds-to-newlayer.py
@@ -0,0 +1,14 @@
+from vanilla.dialogs import *
+from mojo.UI import AskString
+
+newLayer = AskString('Layer to copy all existing glyphs to, e.g. "overlap"')
+
+f = CurrentFont()
+
+glyphsToCopyToNewLayer = f.selectedGlyphNames()
+
+for name in glyphsToCopyToNewLayer:
+    for font in AllFonts():
+        font[name].layers[0].copyLayerToLayer("foreground", newLayer)
+        print(f"Glyphs copied to {newLayer}! Please save font to keep changes.")
+
