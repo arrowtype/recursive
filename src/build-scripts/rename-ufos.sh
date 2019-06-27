156e0e9b1e14ec9a9f094e72ace69ff0ea2b045f
Stephen Nixon <stephen@thundernixon.com>
2019-06-27 15:42:37 -0400

    renaming UFOs for clarity

--- src/build-scripts/rename-ufos.sh
+++ src/build-scripts/rename-ufos.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-find src/* -name '*gradually*' -exec sh -c '
+# Note: spaces must be escaped in git mv, eg:
+# git mv "$file" "${file/'Italic\ -\ gradually\ fixed'/'Slanted'}"
+
+find src/* -name '*Strict*' -exec sh -c '
     for file do
         echo $file
-        git mv "$file" "${file/'Italic\ -\ gradually\ fixed'/'Slanted'}"
+        git mv "$file" "${file/'Strict'/'Linear'}"
     done
 ' sh {} +
