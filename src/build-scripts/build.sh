6e7102a36f7f85478a8a894396bcc93be0543107
Stephen Nixon <stephen@thundernixon.com>
2019-06-28 10:52:19 -0400

    add simple build script

---
+++ src/build-scripts/build.sh
@@ -0,0 +1,28 @@
+# !/bin/bash
+
+set -ex
+source venv/bin/activate
+
+# use designspace as argument
+DS=$1
+
+if [[ -z "$DS" || $DS = "--help" ]] ; then
+    echo 'Add relative path to a designspace file, such as:'
+    echo 'src/build-scripts/build.sh src/masters/mono/FONTNAME.designspace'
+    exit 2
+fi
+
+# ---------------------------------------------------------
+# FontMake ------------------------------------------------
+
+outputDir="font-betas/work-in-progress"
+dsName=$(basename $DS)
+fontName=${dsName/".designspace"/""}
+
+timestamp() {
+  date +"%Y_%m_%d"
+}
+
+date=$(timestamp)
+
+fontmake -m $DS -o variable --output-path $outputDir/$fontName-$date.tff
