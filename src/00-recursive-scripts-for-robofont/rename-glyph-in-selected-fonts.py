ceb0a0749bbac7630a84bbe4b6694e2ac02a3e3b
Stephen Nixon <stephen@thundernixon.com>
2019-07-22 14:13:14 -0400

    adapting glyph-naming script

---
+++ src/00-recursive-scripts-for-robofont/rename-glyph-in-selected-fonts.py
@@ -0,0 +1,75 @@
+# a script to find glyphs with unicode names and give them human-readable names
+# started from https://forum.robofont.com/topic/624/renaming-the-glyph-problem/6
+
+from glyphNameFormatter.reader import u2n, n2u, u2U
+from vanilla.dialogs import *
+
+inputFonts = getFile(
+    "select UFOs", allowsMultipleSelection=True, fileTypes=["ufo"])
+
+
+def updateGlyphName(f):
+    f.features.text = feaText
+
+
+def getNameFromUnicode(g):
+    name = u2n(g.unicode)
+    return name
+
+
+def getReadableNames(font):
+    renamingDict = {}
+    for glyph in font:
+        old_name = glyph.name
+
+        if "uni" in old_name:  # and "." not in old_name:
+            new_name = getNameFromUnicode(glyph)
+
+            print(old_name, "\t\t", new_name)
+            print("---------------------------------")
+
+        if new_name is None:
+            continue
+        else:
+            if new_name not in renamingDict.keys():
+                if new_name != old_name:
+                    renamingDict[old_name] = new_name
+                for glyph2 in font:
+                    if glyph2.name.split(".")[0] == old_name:
+                        old_name2 = glyph2.name
+                        new_name2 = glyph2.name.replace(old_name, new_name)
+                        if new_name2 not in renamingDict.keys():
+                            if new_name2 != old_name2:
+                                renamingDict[old_name2] = new_name2
+    renamingDictKeys = list(renamingDict.keys())
+
+    for old in renamingDictKeys:
+        if old in font.keys():
+            glyph = font[old]
+            if len(glyph.contours) != 0:
+                font.renameGlyph(old, renamingDict[old])
+                for glyph2 in font:
+                    for comp in glyph2.components:
+                        if comp.baseGlyph == old:
+                            comp.baseGlyph = renamingDict[old]
+
+                del renamingDict[old]
+
+    for old in renamingDictKeys:
+        glyph = font[old]
+        if len(glyph.contours) == 0:
+
+            font.renameGlyph(old, renamingDict[old])
+            for glyph2 in font:
+                for comp in glyph2.components:
+                    if comp.baseGlyph == old:
+                        comp.baseGlyph = renamingDict[old]
+
+
+for fontPath in inputFonts:
+    f = OpenFont(fontPath, showInterface=False)
+
+    getReadableNames(f)
+
+    # f.save()
+    # f.close()
