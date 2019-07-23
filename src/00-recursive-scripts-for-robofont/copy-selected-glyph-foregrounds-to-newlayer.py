89f157beeb036a736d80d1dd0bb90501c6e10646
Stephen Nixon <stephen@thundernixon.com>
2019-07-23 19:05:11 -0400

    working to make more glyphs compatible in mono

--- src/00-recursive-scripts-for-robofont/copy-selected-glyph-foregrounds-to-newlayer.py
+++ src/00-recursive-scripts-for-robofont/copy-selected-glyph-foregrounds-to-newlayer.py
@@ -1,5 +1,8 @@
 from vanilla.dialogs import *
+import os
 from mojo.UI import AskString
+
+files =  getFile("Select files to update", allowsMultipleSelection=True, fileTypes=["ufo"])
 
 newLayer = AskString('Layer to copy all existing glyphs to, e.g. "overlap"')
 
@@ -7,8 +10,9 @@ f = CurrentFont()
 
 glyphsToCopyToNewLayer = f.selectedGlyphNames
 
-for name in glyphsToCopyToNewLayer:
-    for font in AllFonts():
+for file in files:
+    font = OpenFont(file)
+    for name in glyphsToCopyToNewLayer:
         font[name].layers[0].copyLayerToLayer("foreground", newLayer)
         print(f"Glyphs copied to {newLayer}! Please save font to keep changes.")
 
