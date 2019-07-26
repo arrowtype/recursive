38e378b1814495271663cf9e4aa91207f5fc1f78
Stephen Nixon <stephen@thundernixon.com>
2019-07-25 23:29:30 -0400

    simplifying overlaps in mono lowercase

--- src/00-recursive-scripts-for-robofont/check-support-layer-for-glyphs.py
+++ src/00-recursive-scripts-for-robofont/check-support-layer-for-glyphs.py
@@ -1,19 +1,29 @@
+
+from vanilla.dialogs import *
+
 glyphsWithSupportLayer = "w"
 
-fontsToCheck = []
+inputFonts = getFile(
+    "select UFOs", allowsMultipleSelection=True, fileTypes=["ufo"])
 
-for f in AllFonts():
-    for layer in f.layers:
-        if layer.name == "support.w.middle":
-            fontsToCheck.append(f)
-            
+
 print("Glyphs that shouldn't be in layer `support.w.middle`:")
 
-for f in fontsToCheck:
-    print("\n",f.info.styleName)
+
+def checkFont(f):
+    print("\n", f.info.styleName)
     for layer in f.layers:
         if layer.name == "support.w.middle":
             for glyphName in layer.keys():
                 if glyphName not in glyphsWithSupportLayer.split(" "):
                     print("  •", glyphName)
+
                     # del layer[glyphName] # NOT WORKING YET
+
+
+for file in inputFonts:
+    f = OpenFont(file)
+    for layer in f.layers:
+        if layer.name == "support.w.middle":
+            checkFont(f)
+    f.close()
