383ccd8e96804511d77339d256a822bb72730781
Stephen Nixon <stephen@thundernixon.com>
2019-07-31 23:41:22 -0400

    work in progress to check glyph widths for consistency across sans

--- src/00-recursive-scripts-for-robofont/check-for-nonconforming-widths.py
+++ src/00-recursive-scripts-for-robofont/check-for-nonconforming-widths.py
@@ -1,3 +1,8 @@
+'''
+    Lets you check that glyphs maintain a similar width unit between all selected UFOs.
+    Assumes you want each glyph to keep the same width between all selected UFOs.
+'''
+
 
 # get filename, check if "mono" or "sans"
 from mojo.UI import OutputWindow
@@ -7,10 +12,14 @@ from mojo.UI import AskString
 
 widthUnit = AskString('Width unit to check for (e.g. 600, 50, 10)')
 
+duplexing = AskString('Check that widths are duplexed between all files? (y/n)')
+
 files = getFile(f"Select files to check glyph widths for units of {widthUnit}",
                 allowsMultipleSelection=True, fileTypes=["ufo"])
 
 badWidthGlyphs = {}
+
+glyphWidthDict = {}
 
 for file in files:
     font = OpenFont(file)
@@ -19,6 +28,13 @@ for file in files:
     for glyph in font:
         if glyph.width % int(widthUnit) != 0:
             badWidthGlyphs[fontName][glyph.name] = glyph.width
+
+    if duplexing.lower() == "y":
+        for glyph in font:
+            if glyph.name not in glyphWidthDict:
+                glyphWidthDict[glyph.name] = []
+            if glyph.name in glyphWidthDict:
+                glyphWidthDict[glyph.name].append(str(glyph.width))
 
     font.close()
 
@@ -36,5 +52,21 @@ for i in badWidthGlyphs.keys():
         print()
 
 
+if duplexing.lower() == "y":
+    nameLength = 15  # max(map(len, glyphWidthDict))
+    print(nameLength)
+    for i in glyphWidthDict.keys():
+
+        if len(set(glyphWidthDict[i])) > 1:
+            print(i.ljust(nameLength), end=" ")
+            for j in glyphWidthDict[i]:
+                print(j, end=" ")
+            print("")
+    #     if len(glyphWidthDict[i].keys()) != 0:
+    #         print(f"\n### {i} – glyphs with different widths between masters\n")
+
+
 # TODO: check for widths that different in a glyph, between masters
     # note... this script might be already written
+
+# TODO: write checks for kerning units (in this or a separate script)
