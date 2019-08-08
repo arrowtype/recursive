1e71e045fbb5cc36b68abda5a1b1ab1f936df245
Stephen Nixon <stephen@thundernixon.com>
2019-08-07 22:29:12 -0400

    really figuring out curve in flipbook animation

--- src/proofs/drawbot-diagrams-etc/flipbook/recursive-flipbook-wave_viz-080619.drawbot.py
+++ src/proofs/drawbot-diagrams-etc/flipbook/recursive-flipbook-wave_viz-080619.drawbot.py
@@ -8,7 +8,7 @@ from fontTools.misc.bezierTools import splitCubicAtT
 # fontFam = "Rec Mono Beta013 Var"
 
 fontFam = "./fonts/Recursive-mono-full--w_ital_slnt-2019_07_25.ttf"
-frames = 100
+frames = 20
 format = "gif"
 
 frontmatter = False
@@ -32,79 +32,26 @@ maxExpression = 1
 
 maxSlant = 0
 minSlant = -15
-
-# def ease(t):
-#     # easingCurve = ((a,a), (b*0.5,a), (b*0.5,b), (b,b))
-#     easingCurve = ((0,0), (pixels/2,0), (pixels/2,pixels), (pixels,pixels))
-#     split = splitCubicAtT(*easingCurve, t)
-#     loc = split[0][-1]
-#     y = loc[1]
-#     return(y)
     
 
 def interp(a, b, t):
     distance = b-a
     return(a + distance * t)
-    
-# # ---------------------------------------------------------
-# # FRONTMATTER ---------------------------------------------
 
-if frontmatter:
-
-    textSize = H/40
-    lineSize = textSize * 1.5
-
-    newPage(W, H)
-    fill(0)
-    rect(0,0,W,H)
-
-    font(fontFam)
-    fontSize(textSize)
-    lineHeight(lineSize)
-    fontVariations(wght=500)
-
-    fill(1)
-    textBox("\
-Recursive is a versatile new variable font from Arrow Type & Google Fonts, inspired by single-stroke casual signpainting but designed for screens. Five variation axes (Proportion, Expression, Weight, Slant, & Italic) allow extreme customizability, while named instances offer predetermined styles for ease of use. \n\n\
-The Proportion axis allows selection between two subfamilies: Mono & Sans (or anything in between). All other axes allow control over font styles without changing the width of glyphs. This allows smooth typographic transitions without affecting line length, enabling new levels of typographic flexibility & interaction.\n\n\
-Recursive is licensed under the SIL Open Font License, making it free & easy to use in any project.\
-    ", (W*0.1, H*0.1, W*0.8, H*0.75))
-
-    # PAGE TWO
-
-    newPage(W, H)
-    fill(0)
-    rect(0,0,W,H)
-
-    font(fontFam)
-    fontSize(textSize)
-    lineHeight(lineSize)
-    fontVariations(wght=500)
-
-    fill(1)
-    textBox("\
-Type design by Stephen Nixon, with contributions from Katja Schimmel, Lisa Huang, and Rafał Buchner, plus early guidance from faculty and visiting instructors at KABK TypeMedia. Type mastering by Ben Kiel.\n\n\
-Started at TypeMedia, 2018. Sponsored by Google Fonts, 2019.\n\n\
-Book design by Math Practice.\n\
-Typeset with Drawbot by Stephen Nixon.\n\n\
-https://recursive.design\n\n\
-Made by friends of Google Fonts (logo)\n\n\
-Recursive Sans \t Arrow Type\
-    ", (W*0.1, H*0.1, W*0.8, H*0.75))
 
 # ---------------------------------------------------------
 # ANIMATION -----------------------------------------------
 
-curve = {}
+# currrveDict = {}
 
-for frame in range(frames):
-    t = frame / frames
-    easingCurve = ((0,0), (pixels/2,0), (pixels/2,pixels), (pixels,pixels))
-    split = splitCubicAtT(*easingCurve, t)
-    loc = split[0][-1]
-    y = loc[1]
+# for frame in range(frames):
+#     t = frame / frames
+#     currrve = ((0,0), (15062, 22), (0,pixels), (pixels,pixels))
+#     split = splitCubicAtT(*currrve, t)
+#     loc = split[0][-1]
+#     y = loc[1]
     
-    curve[t] = y
+#     currrveDict[t] = y
 
 for frame in range(frames):
     
@@ -121,106 +68,147 @@ for frame in range(frames):
     # easingCurve = ((0,0), (pixels/2,0), (pixels/2,pixels), (pixels,pixels))
     # split = splitCubicAtT(*easingCurve, t)
     # loc = split[0][-1]
-    y = curve[t]
+    # y = currrve[t]
     
-    # loc = split[0][-1]
+    # # loc = split[0][-1]
     
-    fill(1)
+    # fill(1)
     
-    completionOnCurve = y / pixels
+    # completionOnCurve = y / pixels
     
-    # completionOnCurve = ease(t)
+    # # completionOnCurve = ease(t)
     
-    # TODO: split into quarters for smoother weight progression
+    # # TODO: split into quarters for smoother weight progression
     
-    # if in first half of frames
-    if frame <= frames*0.5:
+    # # if in first half of frames
+    # if frame <= frames*0.5:
         
-        minXprn = 0.01
-        maxXprn = 1
+    #     minXprn = 0.01
+    #     maxXprn = 1
         
-        minWeight = 300.01
-        maxWeight = 900 - 0.01
+    #     minWeight = 300.01
+    #     maxWeight = 800 - 0.01
         
-        minSlnt = 0.01
-        maxSlnt = -15
+    #     minSlnt = 0.01
+    #     maxSlnt = -15
         
-        currentItal = 0
+    #     currentItal = 0
                 
-        # completionOnCurve = y / H * 0.5
-        completionOnCurve = y / pixels * 2
+    #     # completionOnCurve = y / H * 0.5
+    #     completionOnCurve = y / pixels * 2
         
         
         
-    if frame > frames*0.5:
-        minXprn = 1
-        maxXprn = 0
+    # if frame > frames*0.5:
+    #     minXprn = 1
+    #     maxXprn = 0
         
-        minWeight = 900 - .01
-        maxWeight = 300 + 0.01
+    #     minWeight = 800 - .01
+    #     maxWeight = 300 + 0.01
         
-        minSlnt = -15
-        maxSlnt = 0
-        
-        currentItal = 1
-        
-        completionOnCurve = (y - 0.5) / pixels * 2 -1
-        
-    # if frame > frames * 0.6:
+    #     minSlnt = -15
+    #     maxSlnt = 0
         
     #     currentItal = 1
+        
+    #     completionOnCurve = (y - 0.5) / pixels * 2 -1
+        
+    # # if frame > frames * 0.6:
+        
+    # #     currentItal = 1
     
     
-    currentXprn = interp(minXprn, maxXprn, completionOnCurve)
-    currentWeight = interp(minWeight, maxWeight, completionOnCurve)
-    currentSlnt = interp(minSlnt, maxSlnt, completionOnCurve)
+    # currentXprn = interp(minXprn, maxXprn, completionOnCurve)
+    # currentWeight = interp(minWeight, maxWeight, completionOnCurve)
+    # currentSlnt = interp(minSlnt, maxSlnt, completionOnCurve)
     
-    fontVariations(
-        wght=currentWeight,
-        XPRN=currentXprn,
-        slnt=currentSlnt,
-        ital=currentItal
-        )
+    # # fontVariations(
+    # #     wght=currentWeight,
+    # #     XPRN=currentXprn,
+    # #     slnt=currentSlnt,
+    # #     ital=currentItal
+    # #     )
     
-    print(str(frame).ljust(3), " | factor: ", str(round(completionOnCurve, 3)).ljust(5)," | t: ", str(round(t, 3)).ljust(5), " | wght: ", currentWeight)
+    # # print(str(frame).ljust(3), " | factor: ", str(round(completionOnCurve, 3)).ljust(5)," | t: ", str(round(t, 3)).ljust(5), " | wght: ", currentWeight)
     
-    fontSize(W/1.4)
-    text("rw", (W/15, H/12))
+    # # fontSize(W/1.4)
+    # # text("rw", (W/15, H/12))
     
-    fontSize(W/30)
+    # fontSize(W/30)
     
-    padding = 0.1
-    # text(str(round(currentWeight)), (((W*0.1)+(W*completionOnCurve * 0.7)), H *0.7))
+    # padding = 0.1
+    # # text(str(round(currentWeight)), (((W*0.1)+(W*completionOnCurve * 0.7)), H *0.7))
     
-    x = str('{:4.2f}'.format(currentXprn))
-    w = str('{:3.0f}'.format(currentWeight))
-    s = str('{:5.2f}'.format(currentSlnt))
-    i = str('{:4.2f}'.format(currentItal))
-    text(f"p {str(0)}   x {x}   w {w}   s {s}   i {i}", (((W*0.025)), H *0.025))  
+    # x = str('{:4.2f}'.format(currentXprn))
+    # w = str('{:3.0f}'.format(currentWeight))
+    # s = str('{:5.2f}'.format(abs(currentSlnt)))
+    # i = str('{:4.2f}'.format(currentItal))
+    # text(f"p {str(0)}   x {x}   w {w}   s -{s}   i {i}", (((W*0.025)), H *0.025))  
 
     if debug:
         fill(1,0,1,1)
         size = pixels/pixels * 2
-        rect(0,H*.66, W*y/W, size)      # y - curved
+        # rect(0,H*.66, W*y/W, size)      # y - curved
+        
+        curviness = 0.8
+        
+        def getCurveXY(t):
+            currrve = ((0,0), (pixels*curviness, 22), (pixels-(pixels*curviness),pixels), (pixels,pixels))
+            split = splitCubicAtT(*currrve, t)
+            x,y = split[0][-1][0], split[0][-1][1]
+            return(x,y)
+        
+        
 
-        for frame in range(frames):
+        for i in range(frames):
             
                 
-            fill(1,0,1,1)
-            t = frame / frames
-            size = pixels/pixels * 4
-            oval(loc[0]-size/2, (y*0.375)+(H/12)-(size/2), size, size)
             
-            for frame in range(frames):
-                t = frame / frames
-                # easingCurve = ((0,0), (pixels/2,0), (pixels/2,pixels), (pixels,pixels))
-                # split = splitCubicAtT(*easingCurve, t)
-                # loc = split[0][-1]
-                # y = loc[1]
-                y = curve[t]
-                size = pixels/pixels * 4
-                fill(1,1,1,0.125)
-                oval(loc[0]-size/2, (y*0.375)+(H/12)-(size/2), size, size)
+            fill(1,1,1,0.25)
+            t = i / frames
+            # x = W * t
+            # y = currrveDict[t]
+            
+            
+            
+            
+            # split = splitCubicAtT(*currrve, t)
+            # loc = split[0][-1]
+            # x = loc[0]
+            # y = loc[1]
+            
+            x,y = getCurveXY(t)
+            
+            size = pixels/pixels * 4
+            # oval(x-size/2, (y*0.375)+(H/12)-(size/2), size, size)
+            oval(x-size/2, (y)-(size/2), size, size)
+            
+        fill(1,0,1,1)
+        t = frame / frames
+        # x = W * t
+        # y = currrveDict[t]
+        
+        
+        # currrve = ((0,0), (pixels*steepness, 22), (pixels-(pixels*steepness),pixels), (pixels,pixels))
+        # split = splitCubicAtT(*currrve, t)
+        # x,y = split[0][-1][0], split[0][-1][1]
+        
+        x,y = getCurveXY(t)
+                
+        size = pixels/pixels * 6
+        # oval(x-size/2, (y*0.375)+(H/12)-(size/2), size, size)
+        oval(x-size/2, (y)-(size/2), size, size)
+            
+        # for frame in range(frames):
+        #     t = frame / frames
+        #     # easingCurve = ((0,0), (pixels/2,0), (pixels/2,pixels), (pixels,pixels))
+        #     # split = splitCubicAtT(*easingCurve, t)
+        #     # loc = split[0][-1]
+        #     # y = loc[1]
+        #     y = curve[t]
+        #     size = pixels/pixels * 4
+        #     fill(1,1,1,0.125)
+        #     oval(loc[0]-size/2, (y*0.375)+(H/12)-(size/2), size, size)
             
             
 
