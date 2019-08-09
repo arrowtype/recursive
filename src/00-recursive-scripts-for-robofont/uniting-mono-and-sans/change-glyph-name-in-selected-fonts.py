6f921dad0fc8e8e586bb13a325340c978b28389a
Stephen Nixon <stephen@thundernixon.com>
2019-08-09 01:50:20 -0400

    tiny changes to scripts

--- src/00-recursive-scripts-for-robofont/uniting-mono-and-sans/change-glyph-name-in-selected-fonts.py
+++ src/00-recursive-scripts-for-robofont/uniting-mono-and-sans/change-glyph-name-in-selected-fonts.py
@@ -11,11 +11,9 @@ files = getFile("Select files to update",
                 allowsMultipleSelection=True, fileTypes=["ufo"])
 
 # get currently selected glyphs as a list
-oldName = AskString(
-    'name of glyph to update (only one)').split(" ")
+oldName = AskString('name of glyph to update (only one)').split(" ")
 
-newName = AskString(
-    'new name for glyph').split(" ")
+newName = AskString('new name for glyph').split(" ")
 
 
 # if the user cancels or inputs an empty string, cancel the script
@@ -27,6 +25,15 @@ if len(oldName) > 1 or len(newName) > 1:
 # if the script is valid, keep going
 else:
     for file in files:
-        font = OpenFont(file)
-        font.renameGlyph(oldName[0], newName[0], renameComponents=True,
-                         renameGroups=True, renameKerning=True, renameGlyphOrder=True)
+        font = OpenFont(file, showInterface=False)
+
+        if oldName[0] in font.keys():
+            font.renameGlyph(oldName[0], newName[0], renameComponents=True,
+                            renameGroups=True, renameKerning=True, renameGlyphOrder=True)
+
+            print(f"{oldName[0]} changed to {newName[0]} in {font.info.styleName}")
+        else:
+            print(f"{oldName[0]} not in {font.info.styleName}")
+
+        font.save()
+        font.close()
