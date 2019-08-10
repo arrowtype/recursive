b5b352b7e58ba708f510c6648e5a5973d81ba588
Stephen Nixon <stephen@thundernixon.com>
2019-08-10 01:10:26 -0400

    improving point compatibility check script; cleaning mono fonts

--- src/00-recursive-scripts-for-robofont/checking-similarity-between-fonts/01-check-num_points-selected_fonts.py
+++ src/00-recursive-scripts-for-robofont/checking-similarity-between-fonts/01-check-num_points-selected_fonts.py
@@ -3,10 +3,12 @@ from mojo.UI import AskString
 from mojo.UI import OutputWindow
 import pprint
 
+# TODO: ignore glyphs marked as "experimental" with robofont marx
+# TODO: allow user to paste space-separated string of glyphs to limit check to
 
 ### SET THIS TO IGNORE GLYPHS YOU WANT TO LEAVE ALONE (e.g. experimental glyphs) ###
 
-glyphsToIgnore = ""
+glyphsToIgnore = "R.trap Z.noserif ampersand.code_experimental_2 ampersand.code_experimental_3 ampersand.crossbar at.simple at.short at.replaced_with_prop dagger.daggery g.extra g.long_desc g.longtail g.ss01 onehalf.v1 perthousand r.simple_italic sterling.replaced_with_flat two.replaced_with_rounder zero.ss01 one.flatflag"
 
 ####################################################################################
 
@@ -19,11 +21,19 @@ pointsDict = {}
 OutputWindow().show()
 OutputWindow().clear()
 
+print("\nCOMPATIBILITY CHECK: GLYPH CONSISTENCY & POINT COUNTS\n\n")
+
+print("""\
+    📊 Emoji are present to help you spot point number differences more quickly. 
+    Specific emoji have no meaning as to which numbers are correct or incorrect.
+    The first count will be marked with 🍇, the next with 🍋, and so on.\n
+""")
+
 for file in files:
     font = OpenFont(file, showInterface=False)
 
-    fontName = (font.info.styleName).translate({ord(c): None for c in 'aeiou'})
-    # fontName = (font.info.styleName)
+    # fontName = (font.info.styleName).transl ate({ord(c): None for c in 'aeiou'}) # remove vowels to shorten
+    fontName = (font.info.styleName)
 
     fonts.append(fontName)
 
@@ -68,8 +78,6 @@ for glyphName in pointsDict.keys():
             
 maxFontNameLength = len(max(fonts, key=len))
 
-print(maxFontNameLength)
-
 
 alertEmoji = "🍇 🍋 🍉 🍊 🥑 🍑 🍌 🍒 🌽 🍈 🐶 🐱 🐭 🐰 🦊 🐻 🐯 🦁 🐮 🐷 🐸 🐵 🐔 🐣".split(" ")
 iconDict = {}
@@ -113,7 +121,7 @@ for glyphName in sorted(problemGlyphs):
     # If glyph is not in all fonts, report
     # if len(pointsDict[glyphName]) != len(fonts):
     if glyphName in glyphsNotInAllFonts:
-        print("Glyph is not in all fonts.\n")
+        # print("\tGlyph is not in all fonts.\n")
         print("\tIs in:\n\t------------------")
         for fontName in sorted(fonts):
             if fontName in pointsDict[glyphName]:
@@ -127,22 +135,33 @@ for glyphName in sorted(problemGlyphs):
                 print("\t",fontName)
 
 if len(glyphsWithUnevenPoints) >= 1:
-    print("️\n\n⚠️ Glyphs with unequal points between fonts:")
+    print("️\n\nGlyphs with unequal points between fonts:")
+    print("\t", end=" ")
     for glyphName in sorted(glyphsWithUnevenPoints):
-        print(" - [ ] ", glyphName)
+        # print(" - [ ] ", glyphName)
+        print(glyphName, end=" ")
+    print("")
+
 
 if len(glyphsNotInAllFonts) >= 1:
-    print("\n\n⚠️ Glyphs that aren't in all fonts:")
+    print("\n\nGlyphs that aren't in all fonts:")
+    print("\t", end=" ")
     for glyphName in sorted(glyphsNotInAllFonts):
-        print(" - [ ] ", glyphName)
+        # print(" - [ ] ", glyphName)
+        print(glyphName, end=" ")
+    print("")
 
 if len(problemGlyphs) is 0:
     print("🤖🤖🤖\n")
     print("Looks like all glyphs have the same anchors – nice work! \n")
     print("🎉🎉🎉\n")
 
-if len(glyphsToIgnore.split(" ")) >= 1:
-    print("--------------------------------------------------------------")
-    print("NOTE: ignored the following glyphs (edit script to adjust this)")
+
+if glyphsToIgnore is not "" and len(glyphsToIgnore.split(" ")) > 0:
+    print("\n--------------------------------------------------------------\n")
+    print("NOTE: ignored the following glyphs (they are experiments or currently low-priority)")
+    print("(edit script to adjust these)")
     for name in glyphsToIgnore.split(" "):
-        print("• ", name)
+        print(name, end=" ")
+
+print("\n")
