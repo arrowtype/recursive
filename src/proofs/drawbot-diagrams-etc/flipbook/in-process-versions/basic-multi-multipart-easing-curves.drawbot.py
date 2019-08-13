aea52c1c6ba87a5c053524f9bcd267b51c7756bc
Stephen Nixon <stephen@thundernixon.com>
2019-08-12 23:02:39 -0400

    working on looped curve

--- src/proofs/drawbot-diagrams-etc/flipbook/in-process-versions/basic-multi-multipart-easing-curves.drawbot.py
+++ src/proofs/drawbot-diagrams-etc/flipbook/in-process-versions/basic-multi-multipart-easing-curves.drawbot.py
@@ -15,24 +15,24 @@ if prop is 1:
 else:
     fontFam = "/Users/stephennixon/type-repos/recursive/src/proofs/drawbot-diagrams-etc/flipbook/fonts/Recursive-mono-full--w_ital_slnt-2019_08_12.ttf"
 
-frames = 500 # currently must be in units of 100
+frames = 192 # currently must be in units of 100
 format = "mp4" # pdf, gif, or mp4
 
 bookSize = 3.5 # inches
-DPI = 144 # dots per inch
+DPI = 300 # dots per inch
 pixels = DPI*bookSize
 
-# W, H = pixels, pixels # do not edit this
-# textSize = W/30
+W, H = pixels, pixels # do not edit this
+textSize = W/30
 
-W, H = 1080, 1920
-textSize = W/10
+# W, H = 1080, 1920   # instagram story format
+# textSize = W/10     # instagram story format
 
 rwSize = W/1.4
 
 # curviness = 0.7 # amount of easing steepness. 0 to 1.
 
-padding = W*0.025
+padding = W*0.085714286 # 0.3 inches
 
 # ---------------------------------------------------------
 # ANIMATION -----------------------------------------------
@@ -42,19 +42,43 @@ def interpolate(a, b, t):
     distance = b-a
     return(a + distance * t)
 
-
 # You can even make this getCurve() function do a lot more for you, I'll sketch pseudo code here:
 
-def getAxisValue(t, curviness, axMin, axMax):
-    curve = ((0,0), (W*curviness, 0), (W-(W*curviness),H), (W,H))
+def getWeightValue(t, curviness, axMin, axMax, loop="loop"):
+
+    curve = ((0,0), (0, H*curviness), (W,H-(H*curviness)), (W,H)) # slow to fast to slow (bell curve)
+
+
+
+
+
+def getCurveValue(t, curviness, axMin, axMax, loop="loop"):
+    # curve = ((0,0), (W*curviness, 0), (W-(W*curviness),H), (W,H)) # fast to slow to fast (not sure?)
+    curve = ((0,0), (0, H*curviness), (W,H-(H*curviness)), (W,H)) # slow to fast to slow (bell curve)
     # curve = ((0, 20), (-2689.98, 30), (-2000,H), (W,H))
     split = splitCubicAtT(*curve, t)
     x, y = split[0][-1]
     # Scale the y value to the range of 0 and 1, assuming it was in a range of 0 to 1000
-    f = y / H
-    # Use this value to interpolate between the start and end values on your axis
+    f = x / W
+    
+    # go up with curve for first half, then back down
+    if loop is "loop":
+        if t <= 0.5:
+            f *= 2
+        else:
+            f = 1 - (f - 0.5) * 2
+            
     value = interpolate(axMin, axMax, f)
+            
     return value, x, y
+
+def getItalValue(t):
+    if t <= 0.5:
+        value = 0
+    else:
+        value = 1
+        
+    return value
 
 
 for frame in range(frames):
@@ -70,19 +94,11 @@ for frame in range(frames):
     
 
     t = frame / (frames - 1)
-    # xprnValue = getXprnValue(t)
-    # wghtValue = getWghtValue(t)
-    # slntValue = getSlntValue(t)
-    ## and then go right ahead and draw this frame
-    ## just use these three values to format the text and draw the sliders
 
-    print(t)
-
-    # slntVal = getSlntValue(t)
-    # wghtVal = getWghtValue(t)
-    slntVal = getAxisValue(t, 0.25, 0, -15)
-    wghtVal = getAxisValue(t, 3, 300, 900)
-    xprnVal = getAxisValue(t, 1.4, 0, 1)
+    slntVal = getCurveValue(t, 0, 0, -15)
+    wghtVal = getCurveValue(t, 0.7, 300, 900)
+    xprnVal = getCurveValue(t, 0.5, 0, 1)
+    italVal = getItalValue(t)
 
     fontVariations(wght=wghtVal[0], XPRN=xprnVal[0], slnt=slntVal[0], ital=0.5)
 
@@ -95,25 +111,36 @@ for frame in range(frames):
     # fill(1,0,0)
 
     # rect(0,0,100,100)
-
     fill(1)
-
-    # (y - startPos ) / stepRange
-    # x = W * t
-    # y = -H * ((slntVal - 0) /15)
-    # oval(x-size/2, (y)-(size/2), size, size)
-    # text("yo", (x, y))
-
-    text("yo", (slntVal[1]-W/10, slntVal[2]))
-    # text("slnt\n" + str(round(slntVal[0], 2)), (slntVal[1]-W/10, slntVal[2]))
     
-    # y = H * (((wghtVal - 300) / 600))
-    # oval(x-size/2, (y)-(size/2), size, size)
-    text("nice", (wghtVal[1]-W/10, wghtVal[2]))
-    # text("wght\n" + str(round(wghtVal[0], 0)), (wghtVal[1]-W/10, wghtVal[2]))
+    fontSize(rwSize)
+    text("rw", (W/15, padding*2))
 
-    text("wtf", (xprnVal[1]-W/10, xprnVal[2]))
-    # text("xprn\n" + str(round(xprnVal[0], 2)), (xprnVal[1]-W/10, xprnVal[2]))
+    # page number
+    textBox(str(frame + 1), (padding, padding*0.5, (W- (padding*2)), textSize*1.5), align="right")
+    
+    
+    if debug:
+        print("T: " + str(t))
+        print("-------------------------------------")
+        print("#: " + str(frame))
+        print("x: " + str(round(abs(xprnVal[0]), 0)))
+        print("w: " + str(round(abs(wghtVal[0]), 0)))
+        print("s: " + str(round(abs(slntVal[0]), 2)))
+        print('')
+        
+        fill(0,1,1)
+        fontSize(textSize/2)
+        text("t" + str(round(abs(t), 2)), (W*t, padding))
+        text("s" + str(round(abs(slntVal[0]), 2)), (slntVal[1]-W/10, slntVal[2]+textSize))
+        text("w" + str(round(wghtVal[0], 0)), (wghtVal[1]-W/10, wghtVal[2]))
+        text("x" + str(round(xprnVal[0], 2)), (xprnVal[1]-W/10, xprnVal[2]-textSize))
+
+        x = getCurveValue(t, 0.8, 0, W, loop="nope")[0]
+        size = padding * 0.15
+
+        oval(x-size/2, (padding/2)-(size/2), size, padding)
+
 
 
     
