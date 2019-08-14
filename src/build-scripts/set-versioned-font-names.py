3cccbdff7450115c9a09f69be82ad708a968c420
Stephen Nixon <stephen@thundernixon.com>
2019-08-14 17:32:14 -0400

    start FontTools script to update TTFs with current version

---
+++ src/build-scripts/set-versioned-font-names.py
@@ -0,0 +1,88 @@
+# intended specificall for variable font files
+
+import os
+import argparse
+from fontTools.ttLib import TTFont
+
+def getVersion():
+    with open("version.txt") as f:
+        currentVersion = f.read()
+        return currentVersion
+
+def getFontNameID(font, ID, platformID=3, platEncID=1):
+    name = font['name'].getName(ID, platformID, platEncID)
+    return name
+
+# setName(self, string, nameID, platformID, platEncID, langID)
+
+def setFontNameID(font, ID, newName, platformID=3, platEncID=1, langID=0x409):
+
+    oldName = font['name'].getName(ID, platformID, platEncID)
+    font['name'].setName(newName, ID, platformID, platEncID, langID)
+
+
+    print(f"• name {ID}: '{oldName}' → '{newName}'")
+
+
+
+parser = argparse.ArgumentParser(description='Print out nameID strings of the fonts')
+
+parser.add_argument('fonts', nargs="+")
+
+# parser.add_argument('--id', '-i', default='all')
+
+namesToVersion = {
+    1
+}
+
+NAME_IDS = {
+    1: 'familyName',    # Recursive Sans Linear A
+    3: 'uniqueID',      # 1.005;ARRW;RecursiveSans-LinearA
+    5: 'version',       # Version 1.005
+    6: 'psName',        # RecursiveSans-LinearA
+    16: 'typeFamily'    # Recursive Sans
+}
+
+# name 16
+    # get it
+
+# name 6
+    # split on "-"
+    # replace last part with "Beta{version}"
+
+# name 3 
+    # split on ";"
+    # replace first part with {version}
+    # replace last part with new nameID 6
+
+# name 1 
+    # keep name16, replace rest with version "Beta v{version}"
+
+def main():
+    args = parser.parse_args()
+    projectVersion = getVersion()
+
+    for font_path in args.fonts:
+        ttfont = TTFont(font_path)
+
+        for nameID in NAME_IDS:
+            print(f"{NAME_IDS[nameID].ljust(10)}: {getFontNameID(ttfont, nameID)}")
+
+        famName = getFontNameID(ttfont, 16)
+
+        # get last part of postScript font name, e.g. "LinearA" from "RecursiveMono-LinearA"
+        psName = str(getFontNameID(ttfont, 6))
+        psStyle = psName.split("-")[-1]
+
+        newPsName = psName.replace(psStyle, f"Beta{projectVersion.replace('.','_')}")
+
+        setFontNameID(ttfont, 6, newPsName)
+
+        ttfont.save(font_path + '.fix')
+
+    
+
+
+
+if __name__ == '__main__':
+    main()
