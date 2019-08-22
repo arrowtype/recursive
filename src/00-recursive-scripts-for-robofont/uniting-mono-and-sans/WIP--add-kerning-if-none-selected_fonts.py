94c8393c20c07d2d945313820f6eb6cdabc08e0c
Stephen Nixon <stephen@thundernixon.com>
2019-08-22 15:20:29 -0400

    experimenting with how to add blank kerning to UFOs

--- src/00-recursive-scripts-for-robofont/uniting-mono-and-sans/WIP--add-kerning-if-none-selected_fonts.py
+++ src/00-recursive-scripts-for-robofont/uniting-mono-and-sans/WIP--add-kerning-if-none-selected_fonts.py
@@ -20,6 +20,27 @@ for file in files:
 
     print(fontName)
 
-    print(font.kerning)
+    print(font.groups)
 
+    print(font.kerning.items())
+
+    if len(font.kerning.keys()) == 0:
+        print("no kerns")
+        print(font.kerning)
+
+        help(font.kerning)
+        # font.kerning = {}
+
+        # groups = RKerning()
+
+        groups = RKerning()
+
+        blankKerning = {
+            ('public.kern1.A', 'public.kern2.Z'): 0
+            }
+        font.kerning.update(blankKerning)
+        print(font.kerning)
+
+
+    font.save()
     font.close()
