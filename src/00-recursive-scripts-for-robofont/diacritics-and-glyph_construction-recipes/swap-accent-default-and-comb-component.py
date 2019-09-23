74e95816efbe0cb1f9b242816674351602d3dacd
Stephen Nixon <stephen@thundernixon.com>
2019-09-23 12:53:33 -0400

    make horncomb the drawn version of horn

--- src/00-recursive-scripts-for-robofont/diacritics-and-glyph_construction-recipes/swap-accent-default-and-comb-component.py
+++ src/00-recursive-scripts-for-robofont/diacritics-and-glyph_construction-recipes/swap-accent-default-and-comb-component.py
@@ -2,55 +2,65 @@
 Decomponentize /horncomb, make /horn a component glyph of it.
 
 '''
+from vanilla.dialogs import *
+from glyphConstruction import ParseGlyphConstructionListFromString, GlyphConstructionBuilder
 
-print("hey")
-# from vanilla.dialogs import *
-# from glyphConstruction import ParseGlyphConstructionListFromString, GlyphConstructionBuilder
+files = getFile("Select files to build glyphs in", allowsMultipleSelection=True, fileTypes=["ufo"])
 
-# files = getFile("Select files to build glyphs in", allowsMultipleSelection=True, fileTypes=["ufo"])
+# # the key will use its value as a component
+# # you probably want the "combining" accent to be the value, so it is the source of truth / thing you draw
+# accentsToSwap = {
+#     "horn": "horncomb"
+# }
 
-# # # the key will use its value as a component
-# # # you probably want the "combining" accent to be the value, so it is the source of truth / thing you draw
-# # accentsToSwap = {
-# #     "horn": "horncomb"
-# # }
+txt = '''\
+horn = horncomb ^ 600
+'''
+constructions = ParseGlyphConstructionListFromString(txt)
 
-# txt = '''\
-# horn = horncomb@`center, 400`
-# '''
-# constructions = ParseGlyphConstructionListFromString(txt)
+print(constructions)
 
-# # collect glyphs to ignore if they already exist in the font
-# ignoreExisting = [L.split('=')[0].strip()[1:] for L in txt.split('\n') if L.startswith('?')]
+# collect glyphs to ignore if they already exist in the font
+ignoreExisting = [L.split('=')[0].strip()[1:] for L in txt.split('\n') if L.startswith('?')]
 
-# for file in files:
-#     font = OpenFont(file, showInterface=False)
+def centerGlyph(f,g):
+    totalMargin = f[g].leftMargin + f[g].rightMargin
+    f[g].leftMargin = totalMargin/2
+    f[g].rightMargin = totalMargin/2
 
-#     if "horncomb" in font.keys():
-#         font["horncomb"].decompose()
+for file in files:
+    font = OpenFont(file, showInterface=False)
 
-#     for construction in constructions:
+    if "horncomb" in font.keys():
+        font["horncomb"].decompose()
 
-#         # build a construction glyph
-#         constructionGlyph = GlyphConstructionBuilder(construction, font)
+    for construction in constructions:
 
-#         # if the construction for this glyph was preceded by `?`
-#         # and the glyph already exists in the font, skip it
-#         if constructionGlyph.name in font and constructionGlyph.name in ignoreExisting:
-#             continue
+        # build a construction glyph
+        constructionGlyph = GlyphConstructionBuilder(construction, font)
 
-#         # get the destination glyph in the font
-#         glyph = font.newGlyph(constructionGlyph.name, clear=True)
+        # if the construction for this glyph was preceded by `?`
+        # and the glyph already exists in the font, skip it
+        if constructionGlyph.name in font and constructionGlyph.name in ignoreExisting:
+            continue
 
-#         # draw the construction glyph into the destination glyph
-#         constructionGlyph.draw(glyph.getPen())
+        # get the destination glyph in the font
+        glyph = font.newGlyph(constructionGlyph.name, clear=True)
 
-#         # copy construction glyph attributes to the destination glyph
-#         glyph.name = constructionGlyph.name
-#         glyph.unicode = constructionGlyph.unicode
-#         glyph.width = constructionGlyph.width
-#         glyph.markColor = 0, 0, 0, 0.5
+        # draw the construction glyph into the destination glyph
+        constructionGlyph.draw(glyph.getPen())
 
-#         # if no unicode was given, try to set it automatically
-#         if glyph.unicode is None:
-#             glyph.autoUnicodes()
+        # copy construction glyph attributes to the destination glyph
+        glyph.name = constructionGlyph.name
+        glyph.unicode = constructionGlyph.unicode
+        glyph.width = constructionGlyph.width
+        glyph.markColor = 0, 0, 0, 0.5
+        
+        centerGlyph(font, glyph.name)
+
+        # if no unicode was given, try to set it automatically
+        if glyph.unicode is None:
+            glyph.autoUnicodes()
+            
+    font.save()
+    font.close()
