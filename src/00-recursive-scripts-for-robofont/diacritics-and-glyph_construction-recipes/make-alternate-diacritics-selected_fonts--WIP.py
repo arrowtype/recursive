060279f2016913d6ae8065d4675933cce6f1de27
Stephen Nixon <stephen@thundernixon.com>
2019-10-10 09:18:07 -0400

    build dicts of glyphs w/ relevant suffixes and default glyph diacritics

--- src/00-recursive-scripts-for-robofont/diacritics-and-glyph_construction-recipes/make-alternate-diacritics-selected_fonts--WIP.py
+++ src/00-recursive-scripts-for-robofont/diacritics-and-glyph_construction-recipes/make-alternate-diacritics-selected_fonts--WIP.py
@@ -7,54 +7,137 @@ This script does three primary things:
     → Generates designspace rules (this may be best to split into a separate script... TBD)
 '''
 from vanilla.dialogs import *
+from mojo.UI import OutputWindow
 # from glyphConstruction import ParseGlyphConstructionListFromString, GlyphConstructionBuilder
 
 files = getFile("Select files to build glyphs in", allowsMultipleSelection=True, fileTypes=["ufo"])
+# recipeFile = getFile("Select Glyph Recipes .txt file", allowsMultipleSelection=False, fileTypes=["txt"])
+recipeFile = "/Users/stephennixon/type-repos/recursive/src/00-recursive-scripts-for-robofont/diacritics-and-glyph_construction-recipes/diacritic-recipes-for-recursive.txt"
 
+OutputWindow().show()
+OutputWindow().clear()
 
-## ANCHORS
+## TODO: COPY ANCHORS TO ALTS (if not present)
 # make list of all alternates that are common between fonts
-# make set of base glyphs baseGlyphs
 # copy anchors to alternates, if not already present
+
 alternates = []
-baseGlyphs = []
+altsDict = {}
+defaultGlyphs = []
+defaultGlyphsWithDiacritics = {}
+diacriticsWithDefaultGlyphs = []
 
-# IF suffix is mono, italic, or sans
+relevantAlts = "mono italic sans".split()
 
+# TODO: do these need to be in *all* fonts? check back on it
 def getAlts(f):
     for g in f:
-        if "." in g.name and g.name not in alternates:
+        if "." in g.name and g.name not in alternates and g.name.split(".")[1] in relevantAlts:
             alternates.append(g.name)
 
-def getBases(f):
+        if "." in g.name:
+            base, suffix = g.name.split(".")[0], g.name.split(".")[1]
+            
+            if base not in altsDict.keys() and suffix in relevantAlts:
+                altsDict[base] = []
+            
+            if suffix in relevantAlts and suffix not in altsDict[base]:
+                altsDict[base].append(suffix)
+
+def getDefaultGlyphs(f):
     for alt in alternates:
         base = alt.split(".")[0]
-        if base not in baseGlyphs and base in f.keys():
-            baseGlyphs.append(base)
+        if base not in defaultGlyphs and base in f.keys():
+            defaultGlyphs.append(base)
+
+def getBaseGlyphs(f):
+    for g in f:
+        for comp in g.components:
+            parent = comp.baseGlyph
+            if parent in defaultGlyphs and parent not in defaultGlyphsWithDiacritics.keys():
+                defaultGlyphsWithDiacritics[parent] = []
+            if parent in defaultGlyphs and g.name not in diacriticsWithDefaultGlyphs:
+                defaultGlyphsWithDiacritics[parent].append(g.name)
+
+recipesToCopyForAlts = []
+
+def getRecipesToCopyForAlts(f):
+    with open(recipeFile, 'r') as recipe:
+        for line in recipe:
+            if \
+                '=' in line and \
+                '#' not in line and \
+                line.replace('?','').split('=')[0] in diacriticsWithDefaultGlyphs and \
+                line not in recipesToCopyForAlts:
+
+                recipesToCopyForAlts.append(line)
 
 for file in files:
     f = OpenFont(file, showInterface=False)
 
     getAlts(f)
-    getBases(f)
+    getDefaultGlyphs(f)
+    getBaseGlyphs(f)
+
+    getRecipesToCopyForAlts(f)
 
     f.save()
     f.close()
 
 print("alternates:")
-print(alternates)
+print(sorted(alternates))
 print("")
-print("baseGlyphs:")
-print(baseGlyphs)
+print("defaultGlyphs:")
+print(sorted(defaultGlyphs))
+# print("")
+# print("defaultGlyphsWithDiacritics:")
+# print(sorted(defaultGlyphsWithDiacritics))
+# print("")
+# print("diacriticsWithDefaultGlyphs:")
+# print(sorted(diacriticsWithDefaultGlyphs))
 
-    # for g in f:
-    #     if g not in alternates:
-    #         alternates.append(g)
 
-# for file in files:
-#     f = OpenFont(file, showInterface=False)
-#     for alt in alternates:
-#         if f[alt]
+print("\n--------------------------------------alternates:\n")
+for key in altsDict:
+    print(key,altsDict[key],"\n")
+
+print("\n--------------------------------------defaultGlyphsWithDiacritics:\n")
+for key in defaultGlyphsWithDiacritics:
+    print(key,defaultGlyphsWithDiacritics[key],"\n")
+# print(defaultGlyphsWithDiacritics)
+
+# for L in sorted(recipesToCopyForAlts):
+#     print(L)
+
+
+
+    # you need a dict that has suffixes to diacrtics needed
+        # mono
+            # dotlessi
+                # iacute.mono=dotlessi.mono+acutecomb@top
+                # ibreve.mono=dotlessi.mono+brevecomb@top
+                # icircumflex.mono=dotlessi.mono+circumflexcomb@top
+                # idieresis.mono=dotlessi.mono+dieresiscomb@top
+                # idotbelow.mono=dotlessi.mono+dotbelowcomb@bottom+dotaccentcomb@dotlessi:top
+                # igrave.mono=dotlessi.mono+gravecomb@top
+                # ihook.mono=dotlessi.mono+hookcomb@hook
+                # imacron.mono=dotlessi.mono+macroncomb@top
+                # itilde.mono=dotlessi.mono+tildecomb@top
+            # dotlessj
+                # j.mono=dotlessj.mono+dotaccentcomb@top
+                # jcircumflex.mono=dotlessj.mono+circumflexcomb@top
+            # ETC
+
+
+    # dict glyphAlts
+        # i
+            # mono
+            # italic
+            # sans
+        # m
+            # italic
+        # ETC
+
 
     ## GLYPH CONSTRUCTION
     # baseDiacritics = make list of all component glyphs that contain baseGlyphs
