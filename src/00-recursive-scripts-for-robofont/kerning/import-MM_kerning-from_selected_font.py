d3c8bb2a94850f641153f450fee6d875b8dc2694
Stephen Nixon <stephen@thundernixon.com>
2019-10-18 22:25:11 -0500

    tweaking kerning; starting kern-copy script

--- src/00-recursive-scripts-for-robofont/kerning/import-MM_kerning-from_selected_font.py
+++ src/00-recursive-scripts-for-robofont/kerning/import-MM_kerning-from_selected_font.py
@@ -1,11 +1,29 @@
 from vanilla.dialogs import *
 import metricsMachine
-font = metricsMachine.CurrentFont()
+from mojo.UI import OutputWindow
 
-print(font)
+copyFrom = getFile("Select file to copy from", allowsMultipleSelection=False, fileTypes=["ufo"])[0]
+importTo = getFile("Select files to copy to", allowsMultipleSelection=True, fileTypes=["ufo"])
 
-selectedFile = getFile("Select file to import", allowsMultipleSelection=False, fileTypes=["ufo"])
 
-print(selectedFile)
+OutputWindow().show()
+OutputWindow().clear()
 
-font.kerning.importKerning(selectedFile[0])
+# maybe this could instead export a feature file, and the others would just read that?
+
+for fontPath in importTo:
+    if fontPath != copyFrom:
+        print(fontPath)
+
+        fontToOpen = OpenFont(fontPath, showInterface=False)
+        
+        
+        print(fontToOpen)
+        
+        font = metricsMachine.CurrentFont()
+        font.kerning.importKerning(copyFrom)
+        print(CurrentFont())
+        
+        fontToOpen.save()
+        fontToOpen.close()
+
