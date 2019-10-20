a205efc164ee21ad3e8cdfce345c8fa8fc40ec80
Stephen Nixon <stephen@thundernixon.com>
2019-10-20 15:28:14 -0500

    copy kerning from casual B to C

--- src/00-recursive-scripts-for-robofont/kerning/import-MM_kerning-from_selected_font.py
+++ src/00-recursive-scripts-for-robofont/kerning/import-MM_kerning-from_selected_font.py
@@ -5,25 +5,25 @@ from mojo.UI import OutputWindow
 copyFrom = getFile("Select file to copy from", allowsMultipleSelection=False, fileTypes=["ufo"])[0]
 importTo = getFile("Select files to copy to", allowsMultipleSelection=True, fileTypes=["ufo"])
 
-
 OutputWindow().show()
 OutputWindow().clear()
 
-# maybe this could instead export a feature file, and the others would just read that?
+fontToCopyFrom = OpenFont(copyFrom, showInterface=False)
+
+copyFrom = metricsMachine.MetricsMachineFont(fontToCopyFrom.naked())
 
 for fontPath in importTo:
     if fontPath != copyFrom:
         print(fontPath)
 
         fontToOpen = OpenFont(fontPath, showInterface=False)
-        
-        
         print(fontToOpen)
         
-        font = metricsMachine.CurrentFont()
-        font.kerning.importKerning(copyFrom)
-        print(CurrentFont())
+        copyTo = metricsMachine.MetricsMachineFont(fontToOpen.naked())
+        copyTo.kerning.importKerning(copyFrom)
         
         fontToOpen.save()
         fontToOpen.close()
 
+
+# alternative: maybe this could instead export a feature file, and the others would just read that?
