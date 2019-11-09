cdf8e68e82cd82acef7c5ff67ea8bfbec376f044
Stephen Nixon <stephen@thundernixon.com>
2019-11-09 18:50:29 -0500

    make check script to count glyphnames in groups and check against glyphset in font

---
+++ src/00-recursive-scripts-for-robofont/kerning/check-font_groups-for-glyph_overuse_or_mismatch.py
@@ -0,0 +1,30 @@
+import sys
+from fontParts.world import OpenFont, RFont, RGlyph
+
+groupsPath = sys.argv[1]
+ufoPath = sys.argv[2]
+
+glyphsInGroups = {}
+glyphsInUFOforeground = []
+
+with open(groupsPath, 'r') as file:
+    for line in file:
+        if 'string' in line:
+            line = line.replace('\t','').replace('\n','').replace('<string>','').replace('</string>','')
+            if line not in glyphsInGroups.keys():
+                glyphsInGroups[line] = 1
+            else:
+                glyphsInGroups[line] += 1
+
+font = OpenFont(ufoPath, showInterface=False)
+
+for glyphName in glyphsInGroups.keys():
+    if glyphName not in font.layers[0].keys():
+        print(glyphName)
+
+print(font.layers[0].keys())
+
+## check quantities
+# for key, value in glyphsInGroups.items():
+#     if value > 0:
+#         print(key, value)
