7756690de1d5639d122bc0a2e4295a9fb8648082
Stephen Nixon <stephen@thundernixon.com>
2019-11-11 18:40:33 -0500

    apply unicode to nhookleft to fix build issue #213

--- src/00-recursive-scripts-for-robofont/kerning/check-font_groups-for-glyph_overuse_or_mismatch.py
+++ src/00-recursive-scripts-for-robofont/kerning/check-font_groups-for-glyph_overuse_or_mismatch.py
@@ -1,30 +1,46 @@
 import sys
+import plistlib
 from fontParts.world import OpenFont, RFont, RGlyph
+import os
 
 groupsPath = sys.argv[1]
-ufoPath = sys.argv[2]
+# ufoPath = sys.argv[2]
 
-glyphsInGroups = {}
-glyphsInUFOforeground = []
+# glyphsInGroups = {}
+# glyphsInUFOforeground = []
 
-with open(groupsPath, 'r') as file:
-    for line in file:
-        if 'string' in line:
-            line = line.replace('\t','').replace('\n','').replace('<string>','').replace('</string>','')
-            if line not in glyphsInGroups.keys():
-                glyphsInGroups[line] = 1
-            else:
-                glyphsInGroups[line] += 1
+# with open(groupsPath, 'r') as file:
+#     for line in file:
+#         if 'string' in line:
+#             line = line.replace('\t','').replace('\n','').replace('<string>','').replace('</string>','')
+#             if line not in glyphsInGroups.keys():
+#                 glyphsInGroups[line] = 1
+#             else:
+#                 glyphsInGroups[line] += 1
 
-font = OpenFont(ufoPath, showInterface=False)
+# font = OpenFont(ufoPath, showInterface=False)
 
-for glyphName in glyphsInGroups.keys():
-    if glyphName not in font.layers[0].keys():
-        print(glyphName)
+# for glyphName in glyphsInGroups.keys():
+#     if glyphName not in font.layers[0].keys():
+#         print(glyphName)
 
-print(font.layers[0].keys())
+# print(font.layers[0].keys())
 
-## check quantities
-for key, value in glyphsInGroups.items():
-    if value > 2:
-        print(key, value)
+# ## check quantities
+# for key, value in glyphsInGroups.items():
+#     if value > 2:
+#         print(key, value)
+
+## check keys in plist
+
+glyphsInGroups = []
+
+groupsDict = plistlib.readPlist(os.getcwd() + '/' + groupsPath)
+for key in groupsDict.keys():
+    print(key)
+    for val in groupsDict[key]:
+        glyphsInGroups.append(val)
+
+for glyphName in set(glyphsInGroups):
+    if glyphsInGroups.count(glyphName) > 1:
+        print(glyphName, glyphsInGroups.count(glyphName))
