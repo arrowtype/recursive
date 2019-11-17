203b7609fd6380921a8068db445db21f8419212b
Stephen Nixon <stephen@thundernixon.com>
2019-11-17 13:19:44 -0500

    make UI to copy listed glyphs between mono & sans

--- src/00-recursive-scripts-for-robofont/uniting-mono-and-sans/UI--copy-selected_glyphs-mono_to_sans-selected_fonts.py
+++ src/00-recursive-scripts-for-robofont/uniting-mono-and-sans/UI--copy-selected_glyphs-mono_to_sans-selected_fonts.py
@@ -1,4 +1,5 @@
-from vanilla import FloatingWindow, Button, EditText, TextBox
+from vanilla import FloatingWindow, Button, EditText, TextBox, CheckBox, ColorWell
+from AppKit import NSColor
 from mojo.UI import Message
 from mojo.roboFont import OpenWindow
 from vanilla.dialogs import *
@@ -11,19 +12,28 @@ class GlyphFax(object):
         buttonHeight = 20
         windowWidth = 320
         
-        rows = 4
+        rows = 4.5
         
         self.w = FloatingWindow((windowWidth, buttonHeight*rows + padding*(rows)), "Glyph Fax Machine")
 
         self.fonts = {}
         
-        self.w.textBox = TextBox((x, y, -padding, buttonHeight), "Glyphs to copy (space-separated list)")
+        # self.w.textBox = TextBox((x, y, -padding, buttonHeight), "Glyphs to Copy")
         
-        y += buttonHeight 
+
+        # y += buttonHeight 
         
-        self.w.editText = EditText((x, y, -padding, buttonHeight*2+padding))
+        self.w.editText = EditText((x, y, -padding, buttonHeight*2+padding), placeholder="Space-separated list of glyphs to copy")
+
 
         y += buttonHeight*2 + padding*2
+        
+        self.w.overwriteGlyphsCheckBox = CheckBox((x, y, -padding, buttonHeight), "Overwrite Glyphs", value=False)
+
+
+        self.w.colorWell = ColorWell((windowWidth*0.8, y, -padding, buttonHeight),color=NSColor.orangeColor())
+
+        y += buttonHeight + padding
 
         self.w.sans2mono = Button(
                 (x, y, windowWidth/3 - padding/2, buttonHeight),
@@ -37,22 +47,35 @@ class GlyphFax(object):
 
         self.w.open()
 
-    def copyGlyph(glyphName, fontToCopyFrom, fontToSendTo):
+    def copyGlyph(self,glyphName, fontToCopyFrom, fontToSendTo):
+
+        print(f"copying glyph /{glyphName} from \n\t{fontToCopyFrom} \n\tto \n\t{fontToSendTo}")
+
+        glyphCopyName = glyphName
 
         if glyphName not in fontToSendTo:
             fontToSendTo.newGlyph(glyphName)
+        else:
+            if self.w.overwriteGlyphsCheckBox.get() == True:
+                fontToSendTo[glyphCopyName].clear()
+            else:
+                glyphCopyName = glyphName + '.copy'
+                fontToSendTo.newGlyph(glyphCopyName)
+            # TODO: ask user whether to overwrite glyphs or add suffix to new glyph
 
         glyphToCopy = fontToCopyFrom[glyphName]
-        layerGlyph = fontToSendTo[glyphName].getLayer("foreground")
+        layerGlyph = fontToSendTo[glyphCopyName].getLayer("foreground")
 
         # get the point pen of the layer glyph
         pen = layerGlyph.getPointPen()
         # draw the points of the imported glyph into the layered glyph
         glyphToCopy.drawPoints(pen)
 
-        layerGlyph.width = g.width
+        layerGlyph.width = glyphToCopy.width
 
-        for anchor in layerGlyph.anchors:
+        layerGlyph.markColor = (self.w.colorWell.get().redComponent(), self.w.colorWell.get().greenComponent(),self.w.colorWell.get().blueComponent(),self.w.colorWell.get().alphaComponent())
+
+        for anchor in glyphToCopy.anchors:
             layerGlyph.appendAnchor(anchor.name, (anchor.x, anchor.y))
 
     def glyphsToCopy(self,sender):
@@ -62,7 +85,7 @@ class GlyphFax(object):
         
         return self.w.editText.get().split(" ")
 
-    def getFonts(self, proportionToCopyFrom):
+    def getFontsCueCopying(self, proportionToCopyFrom, glyphsToCopy):
         files = getFile("Select UFOs to copy glyphs between",
                 allowsMultipleSelection=True, fileTypes=["ufo"])
 
@@ -84,14 +107,17 @@ class GlyphFax(object):
                     self.fonts[variation] = [f] + self.fonts[variation]
 
         for variation in self.fonts.keys():
-            print(variation)
+            print('\n',variation,'\n')
 
             for glyphName in glyphsToCopy:
-                copyGlyph(glyphName, self.fonts[variation][0], self.fonts[variation][1])
+                self.copyGlyph(glyphName, self.fonts[variation][0], self.fonts[variation][1])
 
             for f in self.fonts[variation]:
-                print('\t', f)
+                # print('\t', f)
+                f.save()
                 f.close()
+
+        self.w.close()
 
 
     def mono2SansCallback(self, sender):
@@ -100,7 +126,7 @@ class GlyphFax(object):
         glyphsToCopy = self.glyphsToCopy(sender)
 
         if glyphsToCopy is not None:
-            self.getFonts('Mono')
+            self.getFontsCueCopying('Mono',glyphsToCopy)
 
     def sans2monoCallback(self, sender):
         '''Copy glyphs from Sans to Mono masters.'''
@@ -108,7 +134,7 @@ class GlyphFax(object):
         glyphsToCopy = self.glyphsToCopy(sender)
 
         if glyphsToCopy is not None:
-            self.getFonts('Sans')
+            self.getFontsCueCopying('Sans',glyphsToCopy)
 
 
 
