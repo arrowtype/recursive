a9bdba1fdfc86e17a3bf971c7928037aab007ea6
Stephen Nixon <stephen@thundernixon.com>
2019-11-17 23:51:28 -0500

    building new fonts with improved opentype features

--- src/00-recursive-scripts-for-robofont/diacritics-and-glyph_construction-recipes/build_glyphs_recipes-selected_fonts.py
+++ src/00-recursive-scripts-for-robofont/diacritics-and-glyph_construction-recipes/build_glyphs_recipes-selected_fonts.py
@@ -19,13 +19,15 @@ from glyphConstruction import ParseGlyphConstructionListFromString, GlyphConstru
 # apple=.notdef|F8FF
 # '''
 
-txt = ""
-recipeFile = "/Users/stephennixon/type-repos/recursive/src/00-recursive-scripts-for-robofont/diacritics-and-glyph_construction-recipes/diacritic-recipes-for-recursive-generated-with_alts.txt"
-with open(recipeFile, 'r') as recipe:
-    for line in recipe:
-        line = line.replace(' ','')
-        if len(line) > 1:
-            txt += line
+txt = '''\
+zero.ss03 = zero
+'''
+# recipeFile = "/Users/stephennixon/type-repos/recursive/src/00-recursive-scripts-for-robofont/diacritics-and-glyph_construction-recipes/diacritic-recipes-for-recursive-generated-with_alts.txt"
+# with open(recipeFile, 'r') as recipe:
+#     for line in recipe:
+#         line = line.replace(' ','')
+#         if len(line) > 1:
+#             txt += line
 
 print(txt)
 # get the actual glyph constructions from text
