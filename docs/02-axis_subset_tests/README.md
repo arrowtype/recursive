3dcdc812b2a81db2fea4d22a35afdc363f954424
Stephen Nixon <stephen@thundernixon.com>
2019-11-26 21:37:35 -0500

    making test axis-subset fonts, documenting

---
+++ docs/02-axis_subset_tests/README.md
@@ -0,0 +1,25 @@
+# Variable font axis splitting notes
+
+Generally, a variable font's filesize is about:
+
+```
+(the size of a single font) × (number of masters required)
+```
+
+In Recursive, each static instance woff2 is around 40kb.
+
+| File                                            | Description                          | Masters | Size     |
+| ----------------------------------------------- | ------------------------------------ | ------- | -------: |
+| recursive--MONO_0_1.woff2                       | Just the Monospace axis              | 2       |  `62 KB` |
+| recursive--CASL_0_1.woff2                       | Just the Casual axis                 | 2       |  `88 KB` |
+| recursive--slnt_0_15.woff2                      | Slant 0 to -15 (mono 0)              | 2       |  `83 KB` |
+| recursive--slnt_0_15-mono_1.woff2               | Slant 0 to -15 (mono 1)              | 2       |  `82 KB` |
+| recursive--wght_300_800.woff2                   | Weight 300–800                       | 2       |  `80 KB` |
+| recursive--wght_300_1000.woff2                  | Weight 300–1000                      | 3       | `112 KB` |
+| recursive--wght_300_800-mono_0_1.woff2          | Weight 300–800, plus Monospace axis  | 4       | `164 KB` |
+| recursive--wght_300_1000-mono_0_1.woff2         | Weight 300–1000, plus Monospace axis | 4       | `239 KB` |
+| recursive--wght_300_800-slnt0_15.woff2          | Weight 300–800, Slant, Ital 0.5      | 4       | `135 KB` |
+| recursive--wght_300_800-slnt0_15-ital_0.woff2   | Weight 300–800, Slant, Ital 0        | 4       | `135 KB` |
+| recursive--wght_300_1000-slnt0_15.woff2         | Weight 300–1000, Slant, Ital 0.5     | 6       | `194 KB` |
+
+Note: subsetting `ital` makes no filesize difference – all these alternate glyphs must be remaining in the partial font.
--- src/build-scripts/instancing/partial-instance-tests.py
+++ src/build-scripts/instancing/partial-instance-tests.py
@@ -7,17 +7,22 @@ import shutil
 
 
 VF = sys.argv[1]
-outputDir = "font_betas/font_subset_tests"
+outputDir = "docs/02-axis_subset_tests/subset-test-fonts"
 subprocess.check_call(f'mkdir -p {outputDir}'.split())
 
 # keys are file names; values are options for subsetter
 testSubsets = {
-    "wght_300_800": "wght=300:800 CASL=0 MONO=0 slnt=0 ital=0.5",
-    "wght_300_1000": "wght=300:1000 CASL=0 MONO=0 slnt=0 ital=0.5",
-    "CASL_0_1": "CASL=0:1 wght=400 MONO=0 slnt=0 ital=0",
-    "MONO_0_1": "MONO=0:1 wght=400 CASL=0 slnt=0 ital=0",
-    "wght_300_800-slnt0_15": "wght=300:800 slnt=-15:0 MONO=0 CASL=0",
-    "wght_300_800-slnt0_15-ital_05": "wght=300:800 slnt=-15:0 ital=0.5 MONO=0 CASL=0 "
+    "wght_300_800":                 "wght=300:800 CASL=0 MONO=0 slnt=0 ital=0.5",
+    "wght_300_1000":                "wght=300:1000 CASL=0 MONO=0 slnt=0 ital=0.5",
+    "wght_300_800-mono_0_1":       "wght=300:800 slnt=-15:0 ital=0.5 MONO=0:1 CASL=0",
+    "wght_300_1000-mono_0_1":       "wght=300:1000 slnt=-15:0 ital=0.5 MONO=0:1 CASL=0",
+    "CASL_0_1":                     "CASL=0:1 wght=400 MONO=0 slnt=0 ital=0",
+    "MONO_0_1":                     "MONO=0:1 wght=400 CASL=0 slnt=0 ital=0",
+    "wght_300_800-slnt0_15":        "wght=300:800 slnt=-15:0 MONO=0 CASL=0",
+    "wght_300_800-slnt0_15-ital_0": "wght=300:800 slnt=-15:0 ital=0 MONO=0 CASL=0",
+    "wght_300_1000-slnt0_15":       "wght=300:1000 slnt=-15:0 MONO=0 CASL=0",
+    "slnt_0_15":                    "wght=400 slnt=-15:0 ital=0.5 MONO=0 CASL=0",
+    "slnt_0_15-mono_1":             "wght=400 slnt=-15:0 ital=0.5 MONO=1 CASL=0",
 }
 
 
@@ -38,6 +43,6 @@ for key in testSubsets.keys():
 subprocess.check_call(f'mkdir -p {outputDir}/woff2'.split())
 for dir in os.walk(outputDir):
     for file in dir[2]:
-        if "woff2" not in file:
+        if "woff2" not in file and "DS_Store" not in file:
             subprocess.check_call(f"woff2_compress {dir[0]}/{file}".split())
             shutil.move(f"{dir[0]}/{file.replace('.ttf','.woff2')}",f"{dir[0]}/woff2/{file.replace('.ttf','.woff2')}")
