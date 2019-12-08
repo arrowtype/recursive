8a13b986f5c419cc8b5de3722487d847d08c2a05
Stephen Nixon <stephen@thundernixon.com>
2019-12-07 23:35:20 -0500

    trying improved italic x

--- src/build-scripts/build.sh
+++ src/build-scripts/build.sh
@@ -37,7 +37,7 @@ mv $outputDir/$fontName--$date.ttf.fix $outputDir/$fontName--$date.ttf
 woff2_compress $outputDir/$fontName--$date.ttf
 
 ## add subset base64 of woff2 for testing in CodePen, etc
-# pyftsubset $outputDir/$fontName--$date.ttf --unicodes="U+0020-007F" --flavor="woff2" --output-file="$outputDir/$fontName--$date--subset-0020_007F.woff2"
+pyftsubset $outputDir/$fontName--$date.ttf --flavor="woff2" --output-file="$outputDir/$fontName--$date--subset-GF_latin_basic.woff2" --unicodes="U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD"
 
 ## base64 version for CodePen, etc
 # base64 "$outputDir/$fontName--$date--subset-0020_007F.woff2" > "$outputDir/$fontName--$date--subset-0020_007F.base64"
