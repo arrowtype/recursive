82e6f339028bea3ede685b3412b031df4604c118
Stephen Nixon <stephen@thundernixon.com>
2020-01-14 17:55:59 -0500

    set underline and strikethrough values, attempt 1

--- src/01-shell-scripts-for-sources/metrics/set-underline_strikethrough-values.py
+++ src/01-shell-scripts-for-sources/metrics/set-underline_strikethrough-values.py
@@ -1,11 +1,11 @@
 # setting vertical metrics in Recursive
 
 '''
-    Sets underline & strikethrough metrics for all UFOs within a directory.
+	Sets underline & strikethrough metrics for all UFOs within a directory.
 
-    Run of the command line:
-    
-    python src/01-shell-scripts-for-sources/metrics/set-underline_strikethrough-values.py src/masters/mono
+	Run of the command line:
+		
+	python src/01-shell-scripts-for-sources/metrics/set-underline_strikethrough-values.py src/masters/mono
 '''
 
 import sys
@@ -48,72 +48,70 @@ underStrikeVals = {
 		"openTypeOS2StrikeoutSize": 80,
 		"openTypeOS2StrikeoutPosition": 314,
 		"postscriptUnderlineThickness": 150,
-		"postscriptUnderlinePosition": -205,
+		"postscriptUnderlinePosition": -145,
 	},
 }
 
 
 try:
-    if sys.argv[1]:
-        dirToUpdate = sys.argv[1]
-        print(f"Getting UFO paths in dir: {dirToUpdate}")
-        subDirs = next(os.walk(dirToUpdate))[1]
-        ufosToAdjust = [ path for path in subDirs if path.endswith(".ufo")]
+	if sys.argv[1]:
+		dirToUpdate = sys.argv[1]
+		print(f"Getting UFO paths in dir: {dirToUpdate}")
+		subDirs = next(os.walk(dirToUpdate))[1]
+		ufosToAdjust = [ path for path in subDirs if path.endswith(".ufo")]
 
-        head = dirToUpdate
+		head = dirToUpdate
 
 except IndexError:
-    print("Please include path to directory containing UFOs")
+	print("Please include path to directory containing UFOs")
 
 print(ufosToAdjust)
 
 for ufo in sorted(ufosToAdjust):
-    ufoPath = f"{head}/{ufo}"
+	ufoPath = f"{head}/{ufo}"
 
-    font = OpenFont(ufoPath, showInterface=False)
+	font = OpenFont(ufoPath, showInterface=False)
 
-    print('--------------------------------------------------------')
-	styleName = font.info.styleName
-    print(styleName)
+	print('\n\n--------------------------------------------------------')
+	fontName = f"{font.info.familyName} {font.info.styleName}"
+	print(fontName)
 
-	if "Casual A" in styleName or "Casual Light" in styleName:
+	if "Casual A" in fontName or "Casual Light" in fontName:
 		subFam = "Casual A"
-	if "Casual B" in styleName or "Casual Light" in styleName:
+	if "Casual B" in fontName or "Casual Light" in fontName:
 		subFam = "Casual B"
-	if "Casual C" in styleName or "Casual Light" in styleName:
+	if "Casual C" in fontName or "Casual Light" in fontName:
 		subFam = "Casual C"
-	if "Linear A" in styleName or "Casual Light" in styleName:
+	if "Linear A" in fontName or "Casual Light" in fontName:
 		subFam = "Linear A"
-	if "Linear B" in styleName or "Casual Light" in styleName:
+	if "Linear B" in fontName or "Casual Light" in fontName:
 		subFam = "Linear B"
-	if "Linear C" in styleName or "Casual Light" in styleName:
+	if "Linear C" in fontName or "Casual Light" in fontName:
 		subFam = "Linear C"
 
-    print('---------------------------')
-    print('Strikethrough')
+	print('\nStrikethrough ------------------------------------------')
 
-    print("openTypeOS2StrikeoutSize is \t\t", font.info.openTypeOS2StrikeoutSize)
-    print("openTypeOS2StrikeoutPosition is \t\t", font.info.openTypeOS2StrikeoutPosition)
+	print("openTypeOS2StrikeoutSize was \t\t\t", str(font.info.openTypeOS2StrikeoutSize).rjust(6))
+	print("openTypeOS2StrikeoutPosition was \t\t", str(font.info.openTypeOS2StrikeoutPosition).rjust(6))
 
 
-    font.info.openTypeOS2StrikeoutSize = underStrikeVals[subFam]["openTypeOS2StrikeoutSize"]
-    font.info.openTypeOS2StrikeoutPosition = underStrikeVals[subFam]["openTypeOS2StrikeoutPosition"]
+	font.info.openTypeOS2StrikeoutSize = underStrikeVals[subFam]["openTypeOS2StrikeoutSize"]
+	font.info.openTypeOS2StrikeoutPosition = underStrikeVals[subFam]["openTypeOS2StrikeoutPosition"]
 
-    print("openTypeOS2StrikeoutSize is now \t\t", font.info.openTypeOS2StrikeoutSize)
-    print("openTypeOS2StrikeoutPosition is now \t\t", font.info.openTypeOS2StrikeoutPosition)
+	print("openTypeOS2StrikeoutSize is now \t\t", str(font.info.openTypeOS2StrikeoutSize).rjust(6))
+	print("openTypeOS2StrikeoutPosition is now \t\t", str(font.info.openTypeOS2StrikeoutPosition).rjust(6))
 
-	print('---------------------------')
-    print('Underline')
+	print('\nUnderline ----------------------------------------------')
 
-    print("postscriptUnderlineThickness is \t\t", font.info.postscriptUnderlineThickness)
-    print("postscriptUnderlinePosition is \t\t", font.info.postscriptUnderlinePosition)
+	print("postscriptUnderlineThickness was \t\t", str(font.info.postscriptUnderlineThickness).rjust(6))
+	print("postscriptUnderlinePosition was \t\t", str(font.info.postscriptUnderlinePosition).rjust(6))
 
-    font.info.postscriptUnderlineThickness = underStrikeVals[subFam]["postscriptUnderlineThickness"]
-    font.info.postscriptUnderlinePosition = underStrikeVals[subFam]["postscriptUnderlinePosition"]
+	font.info.postscriptUnderlineThickness = underStrikeVals[subFam]["postscriptUnderlineThickness"]
+	font.info.postscriptUnderlinePosition = underStrikeVals[subFam]["postscriptUnderlinePosition"]
 
-    print("postscriptUnderlineThickness is now \t\t", font.info.postscriptUnderlineThickness)
-    print("postscriptUnderlinePosition is now \t\t", font.info.postscriptUnderlinePosition)
+	print("postscriptUnderlineThickness is now \t\t", str(font.info.postscriptUnderlineThickness).rjust(6))
+	print("postscriptUnderlinePosition is now \t\t", str(font.info.postscriptUnderlinePosition).rjust(6))
 
-    font.save()
-    font.close()
+	font.save()
+	font.close()
 
