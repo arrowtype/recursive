1a1680256d89d6856f9e4eef06ceae247a804537
arrowtype <arrowtypeco@gmail.com>
2020-02-03 14:28:01 -0700

    testing vertical metrics for consistency

--- src/01-shell-scripts-for-sources/metrics/set-v_metrics.py
+++ src/01-shell-scripts-for-sources/metrics/set-v_metrics.py
@@ -5,15 +5,19 @@
 
     Run of the command line:
     
-    python src/01-shell-scripts-for-sources/metrics/set-v_metrics.py src/masters/mono
+    python src/01-shell-scripts-for-sources/metrics/set-v_metrics.py src/masters/mono <--save>
 '''
 
 import sys
 import os
 from fontParts.world import *
 
+# new values to save in
 newTopAscenderValue = 950
+newDescenderValue = -250
 
+
+# get font dir
 try:
     if sys.argv[1]:
         print("Getting UFO paths")
@@ -26,8 +30,17 @@ try:
 except IndexError:
     print("Please include directory containing UFOs")
 
-print(ufosToAdjust)
+# check whether to save new results
+save = False
+try:
+    if sys.argv[2] == "-s" or sys.argv[2] == "--save" :
+        print("Saving new vertical metrics to fonts")
+        save = True
 
+except IndexError:
+    print("Dry run. Add second arg of --save or -s to save new vertical metrics.")
+
+# run program
 for ufo in sorted(ufosToAdjust):
     ufoPath = f"{head}/{ufo}"
 
@@ -38,6 +51,8 @@ for ufo in sorted(ufosToAdjust):
     print('--------------------------------------------------------')
     print(font.info.styleName)
 
+    # ascenders
+
     print("openTypeOS2TypoAscender is \t\t", font.info.openTypeOS2TypoAscender)
     print("openTypeHheaAscender is \t\t", font.info.openTypeHheaAscender)
 
@@ -46,6 +61,18 @@ for ufo in sorted(ufosToAdjust):
 
     print("openTypeOS2TypoAscender is now \t\t", font.info.openTypeOS2TypoAscender)
     print("openTypeHheaAscender is now \t\t", font.info.openTypeHheaAscender)
+    
+    # descenders
 
-    font.save()
+    print("openTypeOS2TypoDescender is \t\t", font.info.openTypeOS2TypoDescender)
+    print("openTypeHheaDescender is \t\t", font.info.openTypeHheaDescender)
+
+    font.info.openTypeOS2TypoDescender = newDescenderValue
+    font.info.openTypeHheaDescender = newDescenderValue
+
+    print("openTypeOS2TypoDescender is now \t\t", font.info.openTypeOS2TypoDescender)
+    print("openTypeHheaDescender is now \t\t", font.info.openTypeHheaDescender)
+
+    if save:
+        font.save()
     font.close()
