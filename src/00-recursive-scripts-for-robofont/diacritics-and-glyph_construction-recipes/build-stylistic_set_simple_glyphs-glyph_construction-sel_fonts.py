e1302de54033d87f74f6086a7f2c016c0feed9d2
arrowtype <arrowtypeco@gmail.com>
2020-02-14 16:44:57 -0500

    fix missing diacritic glyphs in stylistic sets for a, g, r, build

--- src/00-recursive-scripts-for-robofont/diacritics-and-glyph_construction-recipes/build-stylistic_set_simple_glyphs-glyph_construction-sel_fonts.py
+++ src/00-recursive-scripts-for-robofont/diacritics-and-glyph_construction-recipes/build-stylistic_set_simple_glyphs-glyph_construction-sel_fonts.py
@@ -7,85 +7,90 @@ from glyphConstruction import ParseGlyphConstructionListFromString, GlyphConstru
 
 # define glyph constructions
 
-# txt = '''\
-# a.simple = a.italic 
-# aacute.simple = aacute.italic 
-# abreve.simple = abreve.italic 
-# abreveacute.simple = abreveacute.italic 
-# abrevedot.simple = abrevedot.italic 
-# abrevegrave.simple = abrevegrave.italic 
-# abrevehook.simple = abrevehook.italic 
-# abrevetilde.simple = abrevetilde.italic 
-# acircumflex.simple = acircumflex.italic 
-# acircumflexacute.simple = acircumflexacute.italic 
-# acircumflexdot.simple = acircumflexdot.italic 
-# acircumflexgrave.simple = acircumflexgrave.italic 
-# acircumflexhook.simple = acircumflexhook.italic 
-# acircumflextilde.simple = acircumflextilde.italic 
-# adieresis.simple = adieresis.italic 
-# adotbelow.simple = adotbelow.italic 
-# agrave.simple = agrave.italic 
-# ahook.simple = ahook.italic 
-# amacron.simple = amacron.italic 
-# aogonek.simple = aogonek.italic 
-# aring.simple = aring.italic 
-# aringacute.simple = aringacute.italic 
-# atilde.simple = atilde.italic
+txt = '''\
+? a.simple = a.italic 
+? aacute.simple = aacute.italic 
+? abreve.simple = abreve.italic 
+? abreveacute.simple = abreveacute.italic 
+? abrevedot.simple = abrevedot.italic 
+? abrevegrave.simple = abrevegrave.italic 
+? abrevehook.simple = abrevehook.italic 
+? abrevetilde.simple = abrevetilde.italic 
+? acircumflex.simple = acircumflex.italic 
+? acircumflexacute.simple = acircumflexacute.italic 
+? acircumflexdot.simple = acircumflexdot.italic 
+? acircumflexgrave.simple = acircumflexgrave.italic 
+? acircumflexhook.simple = acircumflexhook.italic 
+? acircumflextilde.simple = acircumflextilde.italic 
+? adieresis.simple = adieresis.italic 
+? adotbelow.simple = adotbelow.italic 
+? agrave.simple = agrave.italic 
+? ahook.simple = ahook.italic 
+? amacron.simple = amacron.italic 
+? aogonek.simple = aogonek.italic 
+? aring.simple = aring.italic 
+? aringacute.simple = aringacute.italic 
+? atilde.simple = atilde.italic
+ainvertedbreve.simple = ainvertedbreve.italic
+agravedbl.simple = agravedbl.italic
 # #
-# f.simple = f
+? f.simple = f
 # #
-# g.simple = g.italic 
-# gbreve.simple = gbreve.italic 
-# gcaron.simple = gcaron.italic 
-# gcircumflex.simple = gcircumflex.italic 
-# gcommaaccent.simple = gcommaaccent.italic 
-# gdotaccent.simple = gdotaccent.italic
+? g.simple = g.italic 
+? gbreve.simple = gbreve.italic 
+? gcaron.simple = gcaron.italic 
+? gcircumflex.simple = gcircumflex.italic 
+? gcommaaccent.simple = gcommaaccent.italic 
+? gdotaccent.simple = gdotaccent.italic
+gmacron.simple = gmacron.italic
 # #
-# i.simple = i.italic
-# igrave.simple = igrave.italic
-# iacute.simple = iacute.italic
-# icircumflex.simple = icircumflex.italic
-# idieresis.simple = idieresis.italic
-# itilde.simple = itilde.italic
-# imacron.simple = imacron.italic
-# ibreve.simple = ibreve.italic
-# iogonek.simple = iogonek.italic
-# igravedbl.simple = igravedbl.italic
-# iinvertedbreve.simple = iinvertedbreve.italic
-# idieresisacute.simple = idieresisacute.italic
-# ihook.simple = ihook.italic
-# idotbelow.simple = idotbelow.italic
-# idot.simple = idot.italic
-# idotaccent.simple = idotaccent.italic
+? i.simple = i.italic
+? igrave.simple = igrave.italic
+? iacute.simple = iacute.italic
+? icircumflex.simple = icircumflex.italic
+? idieresis.simple = idieresis.italic
+? itilde.simple = itilde.italic
+? imacron.simple = imacron.italic
+? ibreve.simple = ibreve.italic
+? iogonek.simple = iogonek.italic
+? igravedbl.simple = igravedbl.italic
+? iinvertedbreve.simple = iinvertedbreve.italic
+? idieresisacute.simple = idieresisacute.italic
+? ihook.simple = ihook.italic
+? idotbelow.simple = idotbelow.italic
+? idot.simple = idot.italic
+? idotaccent.simple = idotaccent.italic
 # #
-# l.simple = l.italic
-# lacute.simple = lacute.italic
-# lcaron.simple = lcaron.italic
-# lcommaaccent.simple = lcommaaccent.italic
-# ldot.simple = ldot.italic
-# ldotbelow.simple = ldotbelow.italic
-# llinebelow.simple = llinebelow.italic
-# lslash.simple = lslash.italic
-# # lcaron.simple = lcaron.italic
+? l.simple = l.italic
+? lacute.simple = lacute.italic
+? lcaron.simple = lcaron.italic
+? lcommaaccent.simple = lcommaaccent.italic
+? ldot.simple = ldot.italic
+? ldotbelow.simple = ldotbelow.italic
+? llinebelow.simple = llinebelow.italic
+? lslash.simple = lslash.italic
+? lcaron.simple = lcaron.italic
+? lcaron.simple = lcaron.italic
+? ldot.simple = ldot.italic
 # #
-# r.simple = r.italic
-# racute.simple = racute.italic
-# rcaron.simple = rcaron.italic
-# rcommaaccent.simple = rcommaaccent.italic
+? r.simple = r.italic
+? racute.simple = racute.italic
+? rcaron.simple = rcaron.italic
+? rcommaaccent.simple = rcommaaccent.italic
+rinvertedbreve.simple = rinvertedbreve.italic
+rgravedbl.simple = rgravedbl.italic
+rdotbelow.simple = rdotbelow.italic
+rlinebelow.simple = rlinebelow.italic
 # #
-# one.simple = one.sans
+? one.simple = one.sans
 # #
-# zero.slash = zero
-# zerosuperior.slash = zerosuperior
-# zeroinferior.slash = zeroinferior
-# zeroinferior_afrc.slash = zeroinferior.afrc
-# zeroinferior_afrc.slash = zeroinferior.afrc
-# '''
-
-txt='''\
-lcaron.simple = lcaron.italic
-ldot.simple = ldot.italic
+? zero.slash = zero
+? zerosuperior.slash = zerosuperior
+? zeroinferior.slash = zeroinferior
+? zeroinferior_afrc.slash = zeroinferior.afrc
+? zeroinferior_afrc.slash = zeroinferior.afrc
 '''
+
 
 constructions = ParseGlyphConstructionListFromString(txt)
 
