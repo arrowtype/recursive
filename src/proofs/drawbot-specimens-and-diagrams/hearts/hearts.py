a02ef8ae79594dbe48443ea4c7fe14147a5a9e42
arrowtype <arrowtypeco@gmail.com>
2020-02-14 18:03:29 -0500

    make drawbot hearts animation

--- src/proofs/drawbot-specimens-and-diagrams/hearts/hearts.py
+++ src/proofs/drawbot-specimens-and-diagrams/hearts/hearts.py
@@ -29,15 +29,15 @@ outputDir = "exports"
 autoOpen = True
 debug = False
 
-fontFam = f"{currentDir}/Recursive_VF_1.038.fix.ttf" # Update as needed. Easiest when font file is in same directory.
+fontFam = f"{currentDir}/Recursive_VF_1.039.ttf" # Update as needed. Easiest when font file is in same directory.
 
-frames = 2
+frames = 50
 fps = 3
 frameRate = 1/fps # only applicable to mp4 and gif; can be buggy
-fileFormat = "pdf" # pdf, gif, or mp4
+fileFormat = "mp4" # pdf, gif, or mp4
 
 pageSize = 3.5 # inches
-DPI = 72 # dots per inch
+DPI = 150 # dots per inch
 
 paddingInPts = 18
 
@@ -45,7 +45,8 @@ paddingInPts = 18
 # Helper functions
 
 pixels = DPI*pageSize # do not edit
-W, H = pixels, pixels # do not edit
+# W, H = pixels, pixels # do not edit
+W, H = 1080,1080 # do not edit
 padding = DPI*paddingInPts/72 # do not edit
 
 # turn font size into usable value for given pageSize
@@ -57,22 +58,62 @@ def interpolate(a, b, t):
 	return(a + (b-a) * t)
 
 # ----------------------------------------------
-# THE ACTUAL ANIMATION
+# TEXT FUNCTIONS
 
-def drawHearts(r=1,g=1,b=1):
-	fontSize = H*.825
+def writeInfoTop(f, r,g,b):
+	with savedState():
+		fontSizeSm = H*.025
+		fontVariations(wght=500,CASL=1,MONO=1)
+		font(fontFam, fontSizeSm)
+		wghtVal = interpolate(600,400, f)
+		caslVal = interpolate(1, 0, f)
+		fontVariations(wght=wghtVal,CASL=caslVal)
+		casualValue = "{:0.2f}".format(caslVal)
+		blendMode("normal")
+		fill(0)
+		lineHeight(fontSizeSm*1.25)
+
+		wghtVal = interpolate(1000,300, f)
+		caslVal = interpolate(0, 1, f)
+		text(f"CASL = {casualValue}\nwght = {str(int(wghtVal)).rjust(4,'0')}", (W*.75, H*.76), align="center")
+
+		wghtVal = interpolate(300,1000, f)
+		caslVal = interpolate(1, 0, f)
+		text(f"CASL = {casualValue}\nwght = {str(int(wghtVal)).rjust(4,'0')}", (W*.25, H*.275), align="center")
+
+def drawHearts(f, r=1,g=1,b=1, info=False):
+	fontSizeLg = H*.825
 	offset = H*0.0375
 	fill(r,g,b)
-	font(fontFam, fontSize)
+	blendMode("screen")
+	font(fontFam, fontSizeLg)
+	wghtVal = interpolate(1000,300, f)
+	caslVal = interpolate(0, 1, f)
+	fontVariations(wght=wghtVal,CASL=caslVal)
+	fontSize = fontSizeLg
 	text("♥♡", (W/2, 0-offset),align="center")
-	font(fontFam, fontSize)
+
+	wghtVal = interpolate(300, 1000, f)
+	caslVal = interpolate(0, 1, f)
+	fontVariations(wght=wghtVal,CASL=caslVal)
+	fontSize = fontSizeLg
 	text("♡♥", (W/2, H/2-offset*1.5), align="center")
 
+
+# ----------------------------------------------
+# ANIMATION
 
 for frame in range(frames):
 	newPage(W, H) # required for each new page/frame
 	fill(0)
 	rect(0,0,W, H)
+
+	f = frame / frames
+	t = frame / frames
+	if t <= 0.5:
+		f *= 2
+	else:
+		f = 1 - (f - 0.5) * 2
 
 	t = frame / frames
 
@@ -81,12 +122,16 @@ for frame in range(frames):
 		rect(0, H/2, W, 1)
 		rect(W/2, 0, 1, H)
 
-	blendMode("screen")
-	drawHearts(0,1,0)
-	translate(0,H*0.01)
-	drawHearts(1,0,0)
-	translate(0,-H*0.02)
-	drawHearts(0,0,1)
+	
+	rgbOffset = 0.0075
+	
+	drawHearts(f, 0,1,0)
+	translate(0,H*rgbOffset)
+	drawHearts(f, 1,0,0)
+	translate(0,-H*rgbOffset*2)
+	drawHearts(f, 0,0,1, info=True)
+
+	writeInfoTop(f, 0,0,0)
 
 endDrawing() # advised by drawbot docs
 
