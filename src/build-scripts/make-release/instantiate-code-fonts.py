526a5458dab016317d8de5ad068bab42767b1e25
arrowtype <arrowtypeco@gmail.com>
2020-02-21 17:38:05 -0500

    [correctly] generate code-specific font packages & zip

--- src/build-scripts/make-release/instantiate-code-fonts.py
+++ src/build-scripts/make-release/instantiate-code-fonts.py
@@ -31,121 +31,128 @@ import fire
 instanceValues = {
 	# Linear package
 	'Linear': {
-		'MONO': 1,
-		'CASL': 0,
-		'wght': 400,
-		'slnt': 0,
-		'style': 'Regular'
-	},
-	'Linear Italic': {
-		'MONO': 1,
-		'CASL': 0,
-		'wght': 400,
-		'slnt': -10,
-		'style': 'Italic'
-	},
-	'Linear Bold': {
-		'MONO': 1,
-		'CASL': 0,
-		'wght': 700,
-		'slnt': 0,
-		'style': 'Bold'
-	},
-	'Linear Bold Italic': {
-		'MONO': 1,
-		'CASL': 0,
-		'wght': 700,
-		'slnt': -10,
-		'style': 'Bold Italic'
+		'Linear': {
+			'MONO': 1,
+			'CASL': 0,
+			'wght': 400,
+			'slnt': 0,
+			'style': 'Regular'
+		},
+		'Linear Italic': {
+			'MONO': 1,
+			'CASL': 0,
+			'wght': 400,
+			'slnt': -10,
+			'style': 'Italic'
+		},
+		'Linear Bold': {
+			'MONO': 1,
+			'CASL': 0,
+			'wght': 700,
+			'slnt': 0,
+			'style': 'Bold'
+		},
+		'Linear Bold Italic': {
+			'MONO': 1,
+			'CASL': 0,
+			'wght': 700,
+			'slnt': -10,
+			'style': 'Bold Italic'
+		},
 	},
 	# Casual package
 	'Casual': {
-		'MONO': 1,
-		'CASL': 1,
-		'wght': 400,
-		'slnt': 0,
-		'style': 'Regular'
-	},
-	'Casual Italic': {
-		'MONO': 1,
-		'CASL': 1,
-		'wght': 400,
-		'slnt': -10,
-		'style': 'Italic'
-	},
-	'Casual Bold': {
-		'MONO': 1,
-		'CASL': 1,
-		'wght': 700,
-		'slnt': 0,
-		'style': 'Bold'
-	},
-	'Casual Bold Italic': {
-		'MONO': 1,
-		'CASL': 1,
-		'wght': 700,
-		'slnt': -10,
-		'style': 'Bold Italic'
+		'Casual': {
+			'MONO': 1,
+			'CASL': 1,
+			'wght': 400,
+			'slnt': 0,
+			'style': 'Regular'
+		},
+		'Casual Italic': {
+			'MONO': 1,
+			'CASL': 1,
+			'wght': 400,
+			'slnt': -10,
+			'style': 'Italic'
+		},
+		'Casual Bold': {
+			'MONO': 1,
+			'CASL': 1,
+			'wght': 700,
+			'slnt': 0,
+			'style': 'Bold'
+		},
+		'Casual Bold Italic': {
+			'MONO': 1,
+			'CASL': 1,
+			'wght': 700,
+			'slnt': -10,
+			'style': 'Bold Italic'
+		},
 	},
 	# SemiCasual package
 	'SemiCasual': {
-		'MONO': 1,
-		'CASL': 0.5,
-		'wght': 400,
-		'slnt': 0,
-		'style': 'Regular'
-	},
-	'SemiCasual Italic': {
-		'MONO': 1,
-		'CASL': 0.5,
-		'wght': 400,
-		'slnt': -10,
-		'style': 'Italic'
-	},
-	'SemiCasual Bold': {
-		'MONO': 1,
-		'CASL': 0.5,
-		'wght': 700,
-		'slnt': 0,
-		'style': 'Bold'
-	},
-	'SemiCasual Bold Italic': {
-		'MONO': 1,
-		'CASL': 0.5,
-		'wght': 700,
-		'slnt': -10,
-		'style': 'Bold Italic'
+		'SemiCasual': {
+			'MONO': 1,
+			'CASL': 0.5,
+			'wght': 400,
+			'slnt': 0,
+			'style': 'Regular'
+		},
+		'SemiCasual Italic': {
+			'MONO': 1,
+			'CASL': 0.5,
+			'wght': 400,
+			'slnt': -10,
+			'style': 'Italic'
+		},
+		'SemiCasual Bold': {
+			'MONO': 1,
+			'CASL': 0.5,
+			'wght': 700,
+			'slnt': 0,
+			'style': 'Bold'
+		},
+		'SemiCasual Bold Italic': {
+			'MONO': 1,
+			'CASL': 0.5,
+			'wght': 700,
+			'slnt': -10,
+			'style': 'Bold Italic'
+		},
 	},
 	# Duotone package
 	'Duotone': {
-		'MONO': 1,
-		'CASL': 0,
-		'wght': 400,
-		'slnt': 0,
-		'style': 'Regular'
+		'Duotone': {
+			'MONO': 1,
+			'CASL': 0,
+			'wght': 400,
+			'slnt': 0,
+			'style': 'Regular'
+		},
+		'Duotone Italic': {
+			'MONO': 1,
+			'CASL': 1,
+			'wght': 400,
+			'slnt': -10,
+			'style': 'Italic'
+		},
+		'Duotone Bold': {
+			'MONO': 1,
+			'CASL': 0,
+			'wght': 700,
+			'slnt': 0,
+			'style': 'Bold'
+		},
+		'Duotone Bold Italic': {
+			'MONO': 1,
+			'CASL': 1,
+			'wght': 700,
+			'slnt': -10,
+			'style': 'Bold Italic'
+		}
 	},
-	'Duotone Italic': {
-		'MONO': 1,
-		'CASL': 1,
-		'wght': 400,
-		'slnt': -10,
-		'style': 'Italic'
-	},
-	'Duotone Bold': {
-		'MONO': 1,
-		'CASL': 0,
-		'wght': 700,
-		'slnt': 0,
-		'style': 'Bold'
-	},
-	'Duotone Bold Italic': {
-		'MONO': 1,
-		'CASL': 1,
-		'wght': 700,
-		'slnt': -10,
-		'style': 'Bold Italic'
-	}
-	
 }
 
 
@@ -188,51 +195,56 @@ def splitFont(fontPath, outputDirectory="fonts/rec_mono-for-code", newName="Rec 
 
 	
 
-	for instance in instanceValues:
+	for package in instanceValues:
+		for instance in instanceValues[package]:
 
-		print(instance)
+			print(instance)
 
-		instanceFont = instancer.instantiateVariableFont(
-					varfont, {"wght": instanceValues[instance]['wght'], "CASL": instanceValues[instance]['CASL'], "MONO": instanceValues[instance]['MONO'], "slnt": instanceValues[instance]['slnt']}
-				)
-	
-		# UPDATE NAME ID 6, postscript name
-		currentPsName = getFontNameID(instanceFont, 6)
-		newPsName = currentPsName.replace('Sans','').replace(oldName, newName.replace(' ','')).replace('LinearLight',instance.replace(' ',''))
-		setFontNameID(instanceFont, 6, newPsName)
+			instanceFont = instancer.instantiateVariableFont(
+						varfont, {"wght": instanceValues[package][instance]['wght'], "CASL": instanceValues[package][instance]['CASL'], "MONO": instanceValues[package][instance]['MONO'], "slnt": instanceValues[package][instance]['slnt']}
+					)
+		
+			# UPDATE NAME ID 6, postscript name
+			currentPsName = getFontNameID(instanceFont, 6)
+			newPsName = currentPsName.replace('Sans','').replace(oldName, newName.replace(' ','')).replace('LinearLight',instance.replace(' ',''))
+			setFontNameID(instanceFont, 6, newPsName)
 
-		# UPDATE NAME ID 4, full font name
-		currentFullName = getFontNameID(instanceFont, 4)
-		newFullName = currentFullName.replace('Sans','').replace(oldName, newName).replace(' Linear Light',instance)
-		setFontNameID(instanceFont, 4, newFullName)
+			# UPDATE NAME ID 4, full font name
+			currentFullName = getFontNameID(instanceFont, 4)
+			newFullName = currentFullName.replace('Sans','').replace(oldName, newName).replace(' Linear Light',instance)
+			setFontNameID(instanceFont, 4, newFullName)
 
-		# UPDATE NAME ID 3, unique font ID
-		currentUniqueName = getFontNameID(instanceFont, 3)
-		newUniqueName = currentUniqueName.replace('Sans','').replace(oldName, newName.replace(' ','')).replace('LinearLight',instance.replace(' ',''))
-		setFontNameID(instanceFont, 3, newUniqueName)
+			# UPDATE NAME ID 3, unique font ID
+			currentUniqueName = getFontNameID(instanceFont, 3)
+			newUniqueName = currentUniqueName.replace('Sans','').replace(oldName, newName.replace(' ','')).replace('LinearLight',instance.replace(' ',''))
+			setFontNameID(instanceFont, 3, newUniqueName)
 
-		# ADD name 2, style linking name
-		newStyleLinkingName = instanceValues[instance]['style']
-		setFontNameID(instanceFont, 2, newStyleLinkingName)
-		setFontNameID(instanceFont, 17, newStyleLinkingName)
+			# ADD name 2, style linking name
+			newStyleLinkingName = instanceValues[package][instance]['style']
+			setFontNameID(instanceFont, 2, newStyleLinkingName)
+			setFontNameID(instanceFont, 17, newStyleLinkingName)
 
-		# UPDATE NAME ID 1, unique font ID
-		currentFamName = getFontNameID(instanceFont, 1)
-		newFamName = currentFamName.replace(' Sans','').replace(oldName, newName).replace('Linear Light',instance.replace(" " + instanceValues[instance]['style'],''))
-		setFontNameID(instanceFont, 1, newFamName)
-		setFontNameID(instanceFont, 16, newFamName)
+			# UPDATE NAME ID 1, unique font ID
+			currentFamName = getFontNameID(instanceFont, 1)
+			newFamName = currentFamName.replace(' Sans','').replace(oldName, newName).replace('Linear Light',instance.replace(" " + instanceValues[package][instance]['style'],''))
+			setFontNameID(instanceFont, 1, newFamName)
+			setFontNameID(instanceFont, 16, newFamName)
 
 
 
-		# TODO: remove version number from filename?
+			# TODO: remove version number from filename?
 
-		newFileName = fontFileName.replace(oldName,newName.replace(' ','')).replace('_VF','-'+instance.replace(' ',''))
-		outputSubDir = f"{outputDirectory}/{instance.replace(' ','')}"
+			newFileName = fontFileName.replace(oldName,newName.replace(' ','')).replace('_VF_','-'+instance.replace(' ','')+'-')
+			outputSubDir = f"{outputDirectory}/{package}"
 
-		# make dir for new fonts
-		pathlib.Path(outputSubDir).mkdir(parents=True, exist_ok=True) 
+			# make dir for new fonts
+			pathlib.Path(outputSubDir).mkdir(parents=True, exist_ok=True) 
 
-		instanceFont.save(f"{outputSubDir}/{newFileName}")
+			instanceFont.save(f"{outputSubDir}/{newFileName}")
+
+	import shutil
+	shutil.make_archive(f"{outputDirectory}", 'zip', outputDirectory)
+	shutil.move(f"{outputDirectory}.zip",f"{outputDirectory}/{outputDirectory.split('/')[-1]}.zip")
 
 
 
