73d77782815228c58de733509904485fb39f1800
arrowtype <arrowtypeco@gmail.com>
2020-02-23 17:51:27 -0500

    fixing spacing & kerning in Sans punctuation

--- src/00-recursive-scripts-for-robofont/drawing-helpers/slight-sloppy-duplex-current_glyph.py
+++ src/00-recursive-scripts-for-robofont/drawing-helpers/slight-sloppy-duplex-current_glyph.py
@@ -26,28 +26,32 @@ for f in af:
     
     g.leftMargin = totalMargin/2
     g.rightMargin = totalMargin/2
+
+    g.width = width
     
     # print(g.leftMargin, g.rightMargin, g.width, g.name)
     
     #italic offset
+
+    try:
+        if f.info.italicAngle:
+            
+            halfOfGlyph = math.floor((g.bounds[1] + g.bounds[3]) / 2)
+            
+            print(g.bounds, g.bounds[1], g.bounds[-1])
         
-    if f.info.italicAngle:
+            print(halfOfGlyph)
         
-        # halfOfGlyph = math.floor((g.bounds[1] + g.bounds[3]) / 2)
+            italicSlantOffset = math.tan(f.info.italicAngle * math.pi / 180) * (f.info.xHeight * 0.5) # * halfOfGlyph
+            
+            print(italicSlantOffset)
         
-        # print(g.bounds, g.bounds[1], g.bounds[-1])
-    
-        # print(halfOfGlyph)
-    
-        # italicSlantOffset = math.tan(f.info.italicAngle * math.pi / 180) * (f.info.xHeight * 0.5) # * halfOfGlyph
+            # italicOffset = f.lib["com.typemytype.robofont.italicSlantOffset"]
         
-        # print(italicSlantOffset)
-    
-        # # italicOffset = f.lib["com.typemytype.robofont.italicSlantOffset"]
-    
-        # g.moveBy((italicSlantOffset,0))
-        
-        print(g.leftMargin, g.rightMargin)
-    
+            g.moveBy((-italicSlantOffset/2,0))
+            
+            print(g.leftMargin, g.rightMargin)
+    except:
+        print("no italic angle")
     
     
