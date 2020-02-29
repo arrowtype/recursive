2f617ba84611ec22dc96af623da9ad890cf08771
arrowtype <arrowtypeco@gmail.com>
2020-02-29 16:58:14 -0500

    Prep files & readmes for GitHub release

--- src/build-scripts/make-release/00-prep-release.sh
+++ src/build-scripts/make-release/00-prep-release.sh
@@ -1,22 +1,16 @@
-# Recursive-Beta_1.042
-# 	recursive_desktop
-# 		recursive-staticOTF_1.042.otc
-# 		recursive-staticTTF_1.042.ttc
-# 		recursive-variable_1.042.ttf
-# 		rec_mono-for-code
-# 			[rec mono as four ttc]
-# 	recursive_web
-# 		static_woff2
-# 			[TTFs as woff2]
-# 		recursive-variable_1.042.woff2
-# 		subsets
-# 			[figure this out]
-# 
-# copy OFL.txt into folder
-# copy README.md (recommendations) into folder
-
-# run on fonts folder, e.g.
+# run on fonts build folder, e.g.
 # src/build-scripts/make-release/00-prep-release.sh fonts_1.042
+
+
+# ---------------------------------------------
+# CONFIGURATION
+
+desktopDir="Recursive_Desktop"
+desktopCodeDir="Recursive_DesktopCode"
+webDir="Recursive_Web"
+
+# ---------------------------------------------
+# Script setup
 
 set -e
 source venv/bin/activate
@@ -31,73 +25,89 @@ if [[ -z $dir || $dir = "--help" ]] ; then
     exit 2
 fi
 
-# create folder for release
+# make folder name for outputs
 outputDir=Recursive-${version/" "/"_"}
+# clean up past runs
+rm -rf $outputDir
+rm -rf fonts/$outputDir
+rm -rf fonts/$outputDir.zip
+
+# make folders for outputs
 mkdir -p $outputDir
-echo made $outputDir
+mkdir -p $outputDir/$desktopDir
+mkdir -p $outputDir/$desktopCodeDir
+mkdir -p $outputDir/$webDir
 
 
 # ---------------------------------------------
 # copy variable TTF
 
 VF=$dir/Variable_TTF/*.ttf
-cp $VF $outputDir/$(basename $VF)
+cp $VF $outputDir/$desktopDir/$(basename $VF)
 
 # ---------------------------------------------
 # make variable woff2
 
-varFonts=$(ls $dir/Variable_TTF/*.ttf)
+woff2_compress $VF
+fontFile=$(basename $VF)
+woff2file=${fontFile/.ttf/.woff2}
+mkdir -p "$outputDir/$webDir/woff2_variable"
+mv $dir/Variable_TTF/$woff2file $outputDir/$webDir/woff2_variable/$woff2file
 
-for font in $varFonts; do
-	woff2_compress $font
-	fontFile=$(basename $font)
-	woff2file=${fontFile/.ttf/.woff2}
-	mv $dir/Variable_TTF/$woff2file $outputDir/$woff2file
-done
+# ---------------------------------------------
+# make web subsets
+
+# make temp copy of VF ttf
+webVFttf=$outputDir/$webDir/$(basename $VF)
+cp $VF $webVFttf
+
+# make subsets with separate shell script
+src/build-scripts/make-release/unicode_range-subsets.sh $webVFttf
+
+# remove temp variable ttf
+rm $webVFttf
 
 # ---------------------------------------------
 # make TTFs in woff2
 
 ttfFonts=$(ls $dir/Static_TTF/*.ttf)
-mkdir -p $outputDir/static_woff2
+mkdir -p $outputDir/$webDir/woff2_static
 
 for font in $ttfFonts; do
 	woff2_compress $font
 	fontFile=$(basename $font)
 	woff2file=${fontFile/.ttf/.woff2}
-	mv $dir/Static_TTF/$woff2file $outputDir/static_woff2/$woff2file
+	mv $dir/Static_TTF/$woff2file $outputDir/$webDir/woff2_static/$woff2file
 done
 
 # ---------------------------------------------
 # make otc & ttc collections
 
 fonts=$(ls $dir/Static_OTF/*.otf)
-otf2otc $fonts -o "$outputDir/recursive-statics.otc"
+otf2otc $fonts -o "$outputDir/$desktopDir/recursive-statics.otc"
 
 fonts=$(ls $dir/Static_TTF/*.ttf)
-otf2otc $fonts -o "$outputDir/recursive-statics.ttc"
+otf2otc $fonts -o "$outputDir/$desktopDir/recursive-statics.ttc"
 
 # ---------------------------------------------
-# TODO: make code ttc collections
+# Make code-specific fonts
 
-
-
-
-
+python src/build-scripts/make-release/instantiate-code-fonts.py $dir/Variable_TTF/*.ttf -o $outputDir/$desktopCodeDir
 
 # ---------------------------------------------
 # copy metadata
 
 cp OFL.txt $outputDir/LICENSE.txt
-cp $(dirname $0)/release-notes.md $outputDir/README.md
+cp $(dirname $0)/data/release-notes.md $outputDir/README.md
+cp $(dirname $0)/data/rec_mono-for-code--notes.md $outputDir/$desktopCodeDir/README.md
+
 
 # ---------------------------------------------
-# make zip of folder
+# move folder into "fonts/" and make a zip of it
 
-zip $outputDir.zip -r $outputDir
+mv $outputDir fonts
+zip fonts/$outputDir.zip -r fonts/$outputDir
+
 
 # ---------------------------------------------
-# move folder into "fonts/"
-
-cp $outputDir fonts/$outputDir
-cp $outputDir.zip fonts/$outputDir.zip
+# TODO: make subsets for web
