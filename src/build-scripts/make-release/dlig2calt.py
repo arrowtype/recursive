ad2c8bea4aa1bcd6f1caf79d907b8f572d97dfc5
arrowtype <arrowtypeco@gmail.com>
2020-07-22 12:08:56 -0400

    make code ligs 600-width, overflow on left

--- src/build-scripts/make-release/dlig2calt.py
+++ src/build-scripts/make-release/dlig2calt.py
@@ -13,12 +13,17 @@ import fire
 
 
 # TODO: make all glyphs 600 units wide, cropping on left
+    # make dictionary of width units while you do this
+    # advance_sb_pair = 600, 600-prevWidth
 
 # TODO: update dlig code
 
+codeLigs = {}
 
 def dlig2calt(fontPath, inplace=False):
     font = ttLib.TTFont(fontPath)
+
+    unitWidth = font['hmtx']['space'][0] # 600 for most monospace fonts w/ UPM=1000
 
     # make "LIG" glyph
     # __setitem__(self, glyphName, glyph)
@@ -27,12 +32,23 @@ def dlig2calt(fontPath, inplace=False):
     # __setitem__(self, glyphName, advance_sb_pair)
     font['hmtx'].__setitem__('LIG', font['hmtx']['space'])
 
+    for glyphName in font.getGlyphNames():
+        if font['hmtx'][glyphName][0] > 600:
+            print(glyphName, font['hmtx'][glyphName])
+
+            # add to dict for later
+            codeLigs[glyphName] = font['hmtx'][glyphName][0]
+
+            # set width to space (e.g. 600), then offset left side to be negative
+            font['hmtx'].__setitem__(glyphName, (unitWidth, unitWidth - font['hmtx'][glyphName][0]))
+
     # update dlig feature to 'calt'
     featureRecords = font['GSUB'].table.FeatureList.FeatureRecord
     for fea in featureRecords:
         if fea.FeatureTag == 'dlig':
             fea.FeatureTag = 'calt'
             print('Updated feature "dlig" to be "calt".')
+
 
     # save font
     if inplace:
