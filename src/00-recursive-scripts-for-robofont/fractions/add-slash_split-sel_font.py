03120d8d40f0126fbdbda14c17630a21960b92b3
arrowtype <arrowtypeco@gmail.com>
2020-08-21 21:02:57 -0400

    make split fraction work with slash.split

---
+++ src/00-recursive-scripts-for-robofont/fractions/add-slash_split-sel_font.py
@@ -0,0 +1,64 @@
+'''
+    Build slash.split 
+'''
+
+from vanilla.dialogs import *
+from glyphConstruction import ParseGlyphConstructionListFromString, GlyphConstructionBuilder
+
+
+txt = '''\
+slash.split = slash
+'''
+
+print(txt)
+# get the actual glyph constructions from text
+constructions = ParseGlyphConstructionListFromString(txt)
+
+
+
+files = getFile("Select files to build glyphs in", allowsMultipleSelection=True, fileTypes=["ufo"])
+
+# collect glyphs to ignore if they already exist in the font
+ignoreExisting = [L.split('=')[0].strip()[1:] for L in txt.split('\n') if L.startswith('?')]
+
+# Set to False to open fonts with RoboFont UI (e.g. to visually check changes before saving)
+skipInterface = True
+
+for file in files:
+
+    if skipInterface:
+        font = OpenFont(file, showInterface=False)
+    else:
+        font = OpenFont(file, showInterface=True)
+
+    # iterate over all glyph constructions
+    for construction in constructions:
+
+        print(construction)
+        # build a construction glyph
+        constructionGlyph = GlyphConstructionBuilder(construction, font)
+
+        # if the construction for this glyph was preceded by `?`
+        # and the glyph already exists in the font, skip it
+        if constructionGlyph.name in font and constructionGlyph.name in ignoreExisting:
+            continue
+
+        # get the destination glyph in the font
+        glyph = font.newGlyph(constructionGlyph.name, clear=True)
+
+        # draw the construction glyph into the destination glyph
+        constructionGlyph.draw(glyph.getPen())
+
+        # copy construction glyph attributes to the destination glyph
+        glyph.name = constructionGlyph.name
+        glyph.unicode = constructionGlyph.unicode
+        glyph.width = constructionGlyph.width
+        glyph.markColor = 0, 0, 0, 0.5
+
+        # if no unicode was given, try to set it automatically
+        if glyph.unicode is None:
+            glyph.autoUnicodes()
+
+    if skipInterface:
+        font.save()
+        font.close()
