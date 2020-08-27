22e3e94ab2fbfaf2aa9f24e543950b5a85630e89
arrowtype <arrowtypeco@gmail.com>
2020-08-27 12:47:28 -0400

    adding frac/afrc labeling

--- src/proofs/drawbot-specimens-and-diagrams/fractions/show-contextual-fractions.py
+++ src/proofs/drawbot-specimens-and-diagrams/fractions/show-contextual-fractions.py
@@ -62,11 +62,11 @@ def interpolate(a, b, t):
 
 
 font(fontFam, 72)
+fontSizeLg = H*.2825
 
-
-def words(string,frac=False):
+def words(string,frac=False, afrc=False):
     fill(1)
-    fontSizeLg = H*.2825
+    
     font(fontFam, fontSizeLg)
     fontSize(fontSizeLg)
 
@@ -74,6 +74,9 @@ def words(string,frac=False):
 
     if frac:
         openTypeFeatures(frac=True, case=True)
+    
+    if afrc:
+        openTypeFeatures(afrc=True, case=True, ss10=True)
 
     text(string, (W/2, H*0.375),align="center")
 
@@ -85,8 +88,30 @@ def metadata(wghtVal, caslVal, monoVal):
     text(f"CASL: {str(caslVal)}", (padding+W*.33, H*0.1))
     text(f"wght: {str(wghtVal)}", (padding+W*.66, H*0.1))
 
+def fracState(frac=False, afrc=False):
+    font(fontFam, fontSizeLg/3)
+    fontVariations(wght=300,CASL=0, MONO=1)
+    if frac:
+        fill(0,0.1,1)
+        text(f"☑ frac", (padding, H*0.75),align="left")
+        fill(0.25)
+        text(f"☐ afrc", (W/2+padding, H*0.75),align="left")
+    if afrc:
+        fill(0.25)
+        text(f"☐ frac", (padding, H*0.75),align="left")
+        fill(0,0.1,1)
+        text(f"☑ afrc", (W/2+padding, H*0.75),align="left")
+    if frac == False and afrc == False:
+        fill(0.25)
+        text(f"☐ frac", (padding, H*0.75),align="left")
+        text(f"☐ afrc", (W/2+padding, H*0.75),align="left")
 
-def animation(frames, frac=False):
+one = 0
+two = 0
+three = 0
+four = 0
+
+def animation(frames, one, two, three, four, frac=False, afrc=False):
     for frame in range(frames):
         newPage(W,H)
 
@@ -100,25 +125,50 @@ def animation(frames, frac=False):
         else:
             f = 1 - (f - 0.5) * 2
 
-        if frame % 2 == 0:
-            outOfTen = int(frame/2) % 10
-            two   = (outOfTen+1) % 10
-            three = (outOfTen+2) % 10
-            four  = (outOfTen+3) % 10
+        # # first col moves fastest, fourth slowest
+        # if frame % 1 == 0:
+        #     one = int(frame/2) % 10
+        # if frame % 2 == 0:
+        #     two   = (one+1) % 10
+        # if frame % 3 == 0:
+        #     three = (one+2) % 10
+        # if frame % 4 == 0:
+        #     four  = (one+3) % 10
+
+        # update each col every fourth frame
+        if frame % 4 == 0:
+            one = int(frame/2) % 10
+        if (frame+1) % 4 == 0:
+            two   = (one+1) % 10
+        if (frame+2) % 4 == 0:
+            three = (one+2) % 10
+        if (frame+3) % 4 == 0:
+            four  = (one+3) % 10
 
         wghtVal = interpolate(1000,300, f)
         caslVal = interpolate(0, 1, f)
-        monoVal = interpolate(0, 1, f)
+        monoVal = 1
+        if frac or afrc:
+            monoVal = interpolate(0, 1, f)
 
         fontVariations(wght=wghtVal,CASL=caslVal, MONO=monoVal)
-        words(f"{outOfTen}{two}/{three}{four}", frac)
+        words(f"{one}{two}/{three}{four}", frac, afrc)
 
         metadata(wghtVal, caslVal, monoVal)
 
+        fracState(frac,afrc)
 
-animation(frames)
+# TODO: make "slider" visualization of axes
+# TODO: add loop with "afrc"
+# TODO: offset weight & casl from mono
 
-animation(frames, True)
+
+
+animation(frames,one, two, three, four)
+
+animation(frames,one, two, three, four, frac=True)
+
+animation(frames,one, two, three, four, afrc=True)
 
 
 endDrawing()                      # advised by drawbot docs
