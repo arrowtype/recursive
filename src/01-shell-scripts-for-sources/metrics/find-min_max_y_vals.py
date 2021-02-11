e2d9ef51b7ed2f19f8cfdddcedf6b21b590666e2
arrowtype <arrowtypeco@gmail.com>
2021-02-11 14:34:17 -0500

    improve yMin/yMax checking script

--- src/01-shell-scripts-for-sources/metrics/find-min_max_y_vals.py
+++ src/01-shell-scripts-for-sources/metrics/find-min_max_y_vals.py
@@ -12,17 +12,22 @@ from fontParts.world import *
 
 try:
     if sys.argv[1]:
-        dirToUpdate = sys.argv[1]
-        print(f"Getting UFO paths in dir: {dirToUpdate}")
-        subDirs = next(os.walk(dirToUpdate))[1]
+        dirToCheck = sys.argv[1]
+        print(f"Getting UFO paths in dir: {dirToCheck}")
+        subDirs = next(os.walk(dirToCheck))[1]
         ufosToAdjust = [ path for path in subDirs if path.endswith(".ufo")]
 
-        head = dirToUpdate
+        head = dirToCheck
 
 except IndexError:
     print("Please include path to directory containing UFOs")
 
 minY, maxY = 0, 0
+
+tallFont = ""
+tallGlyph = ""
+lowFont = ""
+lowGlyph = ""
 
 print("Finding group min & max Y")
 
@@ -41,13 +46,26 @@ for ufo in sorted(ufosToAdjust):
 
             if glyphMinY < minY:
                 minY = glyphMinY
+                lowFont = f"{font.info.familyName} {font.info.styleName}"
+                lowGlyph = glyph.name
 
             if glyphMaxY > maxY:
                 maxY = glyphMaxY
+                tallFont = f"{font.info.familyName} {font.info.styleName}"
+                tallGlyph = glyph.name
+
+
         except:
             pass
 
 print()
-print(f"Group minY is {minY}")
-print(f"Group maxY is {maxY}")
+print(f"maxY of UFOs in {head} is {maxY}")
+print(f"Font with maxY is {tallFont}")
+print(f"\t in {tallGlyph}")
+print()
+
+print()
+print(f"minY of UFOs in {head} is {minY}")
+print(f"Font with minY is {lowFont}")
+print(f"\t in {lowGlyph}")
 print()
