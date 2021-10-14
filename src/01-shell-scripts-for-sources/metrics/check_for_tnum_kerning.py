2d0cb12c5fec7bab1809ab3fe126ac174238765c
arrowtype <arrowtypeco@gmail.com>
2021-10-14 16:23:27 -0400

    script to check for all kerns with numbers

--- src/01-shell-scripts-for-sources/metrics/check_for_tnum_kerning.py
+++ src/01-shell-scripts-for-sources/metrics/check_for_tnum_kerning.py
@@ -4,13 +4,32 @@
     A known issue is kerning between /seven and /period. But, there may be other kerns beyond that. This will check for them so they can be removed.
 """
 
-from fontParts.fontshell import RFont as Font
+# from fontParts.fontshell import RFont as Font
+from ufoLib2 import Font
+from fontTools.ufoLib.kerning import lookupKerningValue
 
 font = Font("src/ufo/sans/Recursive Sans-Casual A.ufo")
 
 numbers = "zero one two three four five six seven eight nine".split(" ")
 
-for key in font.kerning.keys():
-    if key[0] in numbers or key[1] in numbers:
-        print(key)
+grouped = []
+for value in font.groups.values():
+    for name in value:
+        grouped.append(name)
 
+numberKerns = {}
+
+for number in numbers:
+    numberKerns[number] = []
+    for name in font.keys():
+        kern1 = lookupKerningValue((number, name), font.kerning, font.groups)
+        kern2 = lookupKerningValue((name, number), font.kerning, font.groups)
+
+        if kern1 != 0:
+            numberKerns[number].append(((number, name), kern1))
+        if kern2 != 0:
+            numberKerns[number].append(((name, number), kern2))
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(numberKerns)
