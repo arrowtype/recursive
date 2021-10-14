f9f3618f94199d1bc92df5ffaba6b5d7f51132fb
arrowtype <arrowtypeco@gmail.com>
2021-10-14 16:53:32 -0400

    make one.sans_tnum truly tabular

--- src/01-shell-scripts-for-sources/glyphs/decompose-and-copy-one_sans_tnum.py
+++ src/01-shell-scripts-for-sources/glyphs/decompose-and-copy-one_sans_tnum.py
@@ -3,22 +3,27 @@
 """
 
 import os
-from ufoLib2 import Font
+# from ufoLib2 import Font
+from fontParts.world import OpenFont
 
 sourcesPath = "src/ufo"
+monoDir = os.path.join(sourcesPath, "mono")
+sansDir = os.path.join(sourcesPath, "sans")
 
 monoGlyphToCopyName = "one.sans_tnum"
 
 for path in os.listdir(os.path.join(sourcesPath, "mono")):
     if ".ufo" in path:
-        ufoPath = os.path.join(sourcesPath, path)
+        ufoPath = os.path.join(monoDir, path)
         print(ufoPath)
 
-        font = Font(ufoPath)
+        cwd = os.getcwd()
+
+        font = OpenFont(os.path.join(cwd, ufoPath))
 
         sansPath = ufoPath.replace("mono", "sans").replace("Mono", "Sans")
 
-        sansFont = Font(sansPath)
+        sansFont = OpenFont(os.path.join(cwd, sansPath))
 
         sansFont[monoGlyphToCopyName].clear()
 
@@ -35,4 +40,9 @@ for path in os.listdir(os.path.join(sourcesPath, "mono")):
         # draw the points of the imported glyph into the layered glyph
         glyphToCopy.drawPoints(pen)
 
-        glyphToCopy.markColor(1,1,0,0.5)
+        glyphToCopyTo.width = glyphToCopy.width
+
+        glyphToCopyTo.markColor = (1, 1, 0, 0.5)
+
+        font.save()
+        sansFont.save()
