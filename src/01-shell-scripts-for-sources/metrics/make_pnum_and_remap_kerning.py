f4a058a47cbc6475349abda87a356f2a5a658cd4
arrowtype <arrowtypeco@gmail.com>
2021-10-14 22:48:55 -0400

    script to generate pnum mostly working

--- src/01-shell-scripts-for-sources/metrics/make_pnum_and_remap_kerning.py
+++ src/01-shell-scripts-for-sources/metrics/make_pnum_and_remap_kerning.py
@@ -4,15 +4,12 @@
     - Map kerning currently used for default figures to the pnum
     - Remove kerning from default figures
 
-    Maybe....?
-    1. Change names of default figures (add .pnum suffixes in a way that updates groups & kerning)
-    2. Duplicate new pnum figures as default figures
-    3. Restore unicodes to default figures
-
-    No, that’s too complicated in practice. Instead:
+    Approach:
     1. Duplicate the figures as component glyphs with .pnum suffix (skip /one, because /one.sans exists)
     2. Update all references in kerning, adding .pnum suffix to any number names
     3. Remove kerning references to /one (there are several)
+
+    # TODO? : deal with glyph names that get two periods, e.g. zero.sans.pnum
 
     # TODO: also make one.sans_tnum, with:
     https://github.com/arrowtype/recursive/blob/e84c1bcd9cd077e724b33f70d8215d8314a5c038/src/01-shell-scripts-for-sources/glyphs/decompose-and-copy-one_sans_tnum.py
@@ -25,10 +22,12 @@ monoDir = "src/ufo/mono"
 sansDir = "src/ufo/sans"
 
 # /one is intentionally excluded (one.sans already exists)
-numbers = "zero two three four five six seven eight nine".split(" ")
+numbers = "zero two three four five six seven eight nine six.ss01 nine.ss01 zero.slash zero.dotted zero.sans".split(" ")
 
 # get source UFO paths
-ufoPaths = [os.path.join(monoDir, path) for path in os.listdir(monoDir) if ".ufo" in path] + [os.path.join(sansDir, path) for path in os.listdir(sansDir) if ".ufo" in path]
+# ufoPaths = [os.path.join(monoDir, path) for path in os.listdir(monoDir) if ".ufo" in path] + [os.path.join(sansDir, path) for path in os.listdir(sansDir) if ".ufo" in path]
+
+ufoPaths = ["src/ufo/sans/Recursive Sans-Casual B.ufo"]
 
 for fontPath in ufoPaths:
     print(fontPath)
@@ -49,21 +48,29 @@ for fontPath in ufoPaths:
 
             glyphToCopyTo.markColor = (1, 1, 0, 0.5)
 
+            # make width
+            glyphToCopyTo.width = f[number].width
+
             f.save()
 
         # update glyph names in groups
-        for groupName, members in f.groups.keys():
+        for groupName, members in f.groups.items():
 
             # if a default number is a group
             if number in members:
                 # get members, remove the default number and add the pnum
-                newGroupMembers = [member for member in members].remove(number).append(f'{number}.pnum')
+                groupMembers = [member for member in members]
+                groupMembers.remove(number)
+
+                newGroupMembers = groupMembers + [f'{number}.pnum']
 
                 # delete the group
                 del f.groups[groupName]
 
+                print(newGroupMembers)
+
                 # make new group with adjusted members
-                font.groups[groupName] = newGroupMembers
+                f.groups[groupName] = newGroupMembers
 
         # update glyph names in kerns
         for kern in f.kerning.items():
@@ -73,7 +80,9 @@ for fontPath in ufoPaths:
                 newKern = ((kern[0][0].replace(number,f'{number}.pnum'),kern[0][1].replace(number,f'{number}.pnum')),kern[1])
 
                 # delete old kern
-                del font.kerning[kern[0]]
+                del f.kerning[kern[0]]
 
                 # add in new kern data
-                font.kerning[newKern[0]] = newKern[1]
+                f.kerning[newKern[0]] = newKern[1]
+    
+    f.save()
