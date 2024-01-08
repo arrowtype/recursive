ed67bd773c636b8c40cd645730641a1f00617866
arrowtype <arrowtypeco@gmail.com>
2024-01-08 10:49:51 -0500

    add empty kerning for Greek script to mono sources

--- src/00-recursive-scripts-for-robofont/kerning/add-empty-kerning-if-none.py
+++ src/00-recursive-scripts-for-robofont/kerning/add-empty-kerning-if-none.py
@@ -3,18 +3,22 @@ from mojo.UI import OutputWindow
 import metricsMachine
 
 ## use if you want to select different files
-files = getFile("Select file to copy from", allowsMultipleSelection=True, fileTypes=["ufo"])
+files = getFile(
+    "Select files to add blank kerning to",
+    allowsMultipleSelection=True,
+    fileTypes=["ufo"],
+)
 
 ## uncomment if you wish to see output window
 OutputWindow().show()
 OutputWindow().clear()
 
 for fontPath in files:
-
     f = OpenFont(fontPath, showInterface=False)
 
     if len(f.kerning) == 0:
         f.kerning[("A", "A")] = 0
+        f.kerning[("pi", "pi")] = 0
         # del f.kerning[("A","A")]
 
     print(f.info.styleName)
